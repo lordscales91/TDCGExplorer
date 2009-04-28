e4dabd4bf549c122f68b7899b5527467ebea535c
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-04-28 03:39:11 +0000

    added tah

--- trunk/marimo/config/routes.rb
+++ trunk/marimo/config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :tahs
+
   map.resources :arcs
 
   # The priority is based upon order of creation: first created -> highest priority.
