04879b349673c85ca2f3a683d0ffc293a610bc92
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-04-29 19:53:02 +0000

    added gettext

--- marimo/lib/authenticated_system.rb
+++ marimo/lib/authenticated_system.rb
@@ -5,7 +5,7 @@ module AuthenticatedSystem
     end
 
     def current_user
-      @current_user = User.find(session[:user_id]) if session[:user_id]
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
     def current_user=(user)
