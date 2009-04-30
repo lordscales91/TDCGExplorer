8b5f6faca9e16e7443171f3de96e985582dc107d
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-04-30 18:43:25 +0000

    added

---
+++ marimo/bin/arcsummary_from_tahpath.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby -KS
+require File.dirname(__FILE__) + "/../config/environment"
+
+Arc.find(:all, :conditions => "summary is null").each do |arc|
+  tah = arc.tahs.first
+  if tah && tah.path
+    md = %r([^/_]+).match(tah.path)
+    folder = md[0]
+    folder.sub!(/\.tah$/, '')
+    puts [arc.id, folder, arc.origname].join("\t")
+  end
+end
