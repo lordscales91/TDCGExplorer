975214087bc9bcb38a9d6a3c6e069f2a1001a974
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-04-30 02:55:59 +0000

    tah_hash as hex

--- marimo/app/models/tso.rb
+++ marimo/app/models/tso.rb
@@ -8,7 +8,7 @@ class Tso < ActiveRecord::Base
   belongs_to :tah
 
   def before_save
-    self.tah_hash = TAHHash.calc(path) if defined? TAHHash
+    self.tah_hash = '%08X' % TAHHash.calc(path) if defined? TAHHash
     self.tah_hash ||= ''
     nil
   end
