b996c7837e769ccf98198ac346a56857a0889eeb
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-04-30 02:28:35 +0000

    add tah_hash to tso

--- marimo/app/models/tso.rb
+++ marimo/app/models/tso.rb
@@ -1,3 +1,15 @@
+begin
+  require "tahhash.so"
+rescue LoadError
+  #
+end
+
 class Tso < ActiveRecord::Base
   belongs_to :tah
+
+  def before_save
+    self.tah_hash = TAHHash.calc(path) if defined? TAHHash
+    self.tah_hash ||= ''
+    nil
+  end
 end
