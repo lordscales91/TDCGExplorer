8e9bafb68616641324670932ece6145de18f66c0
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-02 18:02:16 +0000

    arc relations

--- marimo/spec/models/relationship_spec.rb
+++ marimo/spec/models/relationship_spec.rb
@@ -1,14 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Relationship do
-  before(:each) do
-    @valid_attributes = {
-      :kind => 1,
-      :note => "value for note"
-    }
+  fixtures :arcs, :relationships
+
+  it "one ‚ÍŠÖŒW«‚ğ‚Â" do
+    arcs(:one).relationships.should == [ relationships(:one) ]
   end
 
-  it "should create a new instance given valid attributes" do
-    Relationship.create!(@valid_attributes)
+  it "one ‚Í two ‚ÆŠÖŒW‚ğ‚Â" do
+    arcs(:one).relations.should == [ arcs(:two) ]
+  end
+
+  it "two ‚Í‹tŠÖŒW«‚ğ‚Â" do
+    arcs(:two).rev_relationships.should == [ relationships(:one) ]
+  end
+
+  it "two ‚Í one ‚Æ‹tŠÖŒW‚ğ‚Â" do
+    arcs(:two).rev_relations.should == [ arcs(:one) ]
   end
 end
