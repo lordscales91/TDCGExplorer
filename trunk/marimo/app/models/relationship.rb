549bc69176ab46dc43ec1f1da4485d962315afab
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-02 19:00:16 +0000

    display arc relations

--- trunk/marimo/app/models/relationship.rb
+++ trunk/marimo/app/models/relationship.rb
@@ -1,4 +1,26 @@
 class Relationship < ActiveRecord::Base
   belongs_to :from, :class_name => 'Arc'
   belongs_to :to, :class_name => 'Arc'
+
+  def kind_caption
+    case kind
+    when 1
+      '重複'
+    when 2
+      '新版'
+    when 3
+      '前提'
+    end
+  end
+
+  def rev_kind_caption
+    case kind
+    when 1
+      '重複'
+    when 2
+      '旧版'
+    when 3
+      '提供'
+    end
+  end
 end
