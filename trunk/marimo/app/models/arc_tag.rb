64918118c71f67a77002e58b77b89e719c8f47b9
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-03 03:34:41 +0000

    added arc_tag

---
+++ trunk/marimo/app/models/arc_tag.rb
@@ -0,0 +1,2 @@
+class ArcTag < ActiveRecord::Base
+end
---
+++ trunk/marimo/spec/models/arc_tag_spec.rb
@@ -0,0 +1,14 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ArcTag do
+  before(:each) do
+    @valid_attributes = {
+      :arc => ,
+      :tag => 
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    ArcTag.create!(@valid_attributes)
+  end
+end
