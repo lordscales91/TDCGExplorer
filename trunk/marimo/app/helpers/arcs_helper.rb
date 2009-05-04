4dbc31c4a39659244b54661a367c0f60f5e11b11
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-04 02:58:23 +0000

    edit arc equips

--- trunk/marimo/app/helpers/arcs_helper.rb
+++ trunk/marimo/app/helpers/arcs_helper.rb
@@ -1,2 +1,7 @@
 module ArcsHelper
+  def add_arc_equip_link(name)
+    link_to_function name do |page|
+      page.insert_html :bottom, :arc_equips, :partial => 'arc_equip', :object => ArcEquip.new
+    end
+  end
 end
