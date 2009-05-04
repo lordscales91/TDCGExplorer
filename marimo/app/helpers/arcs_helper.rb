d8154bda7216813c15749f9179e7dcb2f28bc462
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-04 03:54:35 +0000

    edit arc relations

--- marimo/app/helpers/arcs_helper.rb
+++ marimo/app/helpers/arcs_helper.rb
@@ -4,4 +4,10 @@ module ArcsHelper
       page.insert_html :bottom, :arc_equips, :partial => 'arc_equip', :object => ArcEquip.new
     end
   end
+
+  def add_relationship_link(name)
+    link_to_function name do |page|
+      page.insert_html :bottom, :relationships, :partial => 'relationship', :object => Relationship.new
+    end
+  end
 end
