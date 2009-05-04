a44815429b33a2983d86ea95cfa145322c663103
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-04 08:56:25 +0000

    welcome search

--- trunk/marimo/app/controllers/welcome_controller.rb
+++ trunk/marimo/app/controllers/welcome_controller.rb
@@ -3,6 +3,16 @@ class WelcomeController < ApplicationController
   layout 'anon'
 
   def index
+    @search = Welcome::Search.new(params[:search])
+    if @search.text.blank?
+      @arcs = []
+      @tahs = []
+    else
+      @arc_search = Arc::Search.new(params[:search])
+      @arcs = Arc.paginate(@arc_search.find_options.merge(:page => nil, :order => 'location, code'))
+      @tah_search = Tah::Search.new(params[:search])
+      @tahs = Tah.paginate(@tah_search.find_options.merge(:page => nil))
+    end
   end
 
 end
