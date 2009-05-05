861a89db9e38370a134f9c8a7fbe0c18ab42e13f
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-05 05:38:25 +0000

    login required on destroy

--- trunk/marimo/app/controllers/tsos_controller.rb
+++ trunk/marimo/app/controllers/tsos_controller.rb
@@ -1,7 +1,7 @@
 class TsosController < ApplicationController
   include AuthenticatedSystem
   layout 'anon'
-  before_filter :login_required, :only => [ :new, :edit, :create, :update ]
+  before_filter :login_required, :only => [ :new, :edit, :create, :update, :destroy ]
 
   # GET /tsos
   # GET /tsos.xml
