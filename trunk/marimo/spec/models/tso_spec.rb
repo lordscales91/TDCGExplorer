b9995f62fb4ba160c3ebd2aafbcb0c885da988e2
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-09 06:02:44 +0000

    revised fixtures

--- trunk/marimo/spec/models/tso_spec.rb
+++ trunk/marimo/spec/models/tso_spec.rb
@@ -3,18 +3,18 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Tso, "search" do
   fixtures :tsos
 
-  it "path N001BODY_A00.tso で検索すると one にマッチする" do
-    @search = Tso::Search.new('path' => 'N001BODY_A00.tso')
+  it "path data/model/N400BODY_A00.tso で検索すると one にマッチする" do
+    @search = Tso::Search.new('path' => 'data/model/N400BODY_A00.tso')
     Tso.find(:all, @search.find_options).should == [ tsos(:one) ]
   end
 
-  it "path N001BODY_A00 で検索すると one にマッチする" do
-    @search = Tso::Search.new('path' => 'N001BODY_A00')
+  it "path data/model/N400BODY_A00 で検索すると one にマッチする" do
+    @search = Tso::Search.new('path' => 'data/model/N400BODY_A00')
     Tso.find(:all, @search.find_options).should == [ tsos(:one) ]
   end
 
-  it "path N001BODY で検索すると one two にマッチする" do
-    @search = Tso::Search.new('path' => 'N001BODY')
+  it "path data/model/N400BODY で検索すると one two にマッチする" do
+    @search = Tso::Search.new('path' => 'data/model/N400BODY_A')
     Tso.find(:all, @search.find_options).should == [ tsos(:one), tsos(:two) ]
   end
 end
