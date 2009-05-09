f3458a6708d262caf2b2cc7c692ba8737c632505
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-09 05:35:38 +0000

    tsos search path spec

---
+++ trunk/marimo/spec/models/tso_spec.rb
@@ -0,0 +1,20 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Tso, "search" do
+  fixtures :tsos
+
+  it "path N001BODY_A00.tso で検索すると one にマッチする" do
+    @search = Tso::Search.new('path' => 'N001BODY_A00.tso')
+    Tso.find(:all, @search.find_options).should == [ tsos(:one) ]
+  end
+
+  it "path N001BODY_A00 で検索すると one にマッチする" do
+    @search = Tso::Search.new('path' => 'N001BODY_A00')
+    Tso.find(:all, @search.find_options).should == [ tsos(:one) ]
+  end
+
+  it "path N001BODY で検索すると one two にマッチする" do
+    @search = Tso::Search.new('path' => 'N001BODY')
+    Tso.find(:all, @search.find_options).should == [ tsos(:one), tsos(:two) ]
+  end
+end
