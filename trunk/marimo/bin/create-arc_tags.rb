5f3cceb31110be013622201a4c9992648f7df317
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-09 14:33:47 +0000

    revised

--- trunk/marimo/bin/create-arc_tags.rb
+++ trunk/marimo/bin/create-arc_tags.rb
@@ -3,24 +3,36 @@ require File.dirname(__FILE__) + "/../config/environment"
 
 while line = gets
   code, equip_caption, tag_caption, summary = line.chomp.split(/\t/)
+  next unless tag_caption
+
   unless summary.nil?
     summary.strip!
     if md = /"(.+?)"/.match(summary)
       summary = md[1]
     end
   end
+
   arc = Arc.find_by_code(code)
-  if arc
-    # puts "#{arc.code}:#{arc.summary}:#{summary}"
+  unless arc
+    puts "code not found. #{code}"
+    next
+  end
+
+  # puts "#{arc.code}:#{arc.summary}:#{summary}"
+  unless summary.nil?
     arc.summary = summary
     arc.save!
   end
-  if arc && tag_caption
-    tag_names = tag_caption.split('/')
-    tag_names.each do |tag_name|
-      tag = Tag.find_or_create_by_name(tag_name)
-      ArcTag.create(:arc_id => arc.id, :tag_id => tag.id)
-      puts "created arc_tag arc.code=#{code} tag.name=#{tag.name}"
-    end
+
+  old_tag_names = arc.tags.map { |t| t.name }
+
+  tag_names = tag_caption.split('/')
+  add_names = tag_names - old_tag_names
+  sub_names = old_tag_names - tag_names
+
+  add_names.each do |tag_name|
+    tag = Tag.find_or_create_by_name(tag_name)
+    arc_tag = ArcTag.create(:arc_id => arc.id, :tag_id => tag.id)
+    puts "created arc_tag arc.code=#{code} tag.name=#{tag.name}"
   end
 end
