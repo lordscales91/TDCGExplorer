a179c30b3441a936b64e1ace55c6b69ebf57ab78
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-09 06:27:45 +0000

    arcs search text spec

--- marimo/spec/models/arc_spec.rb
+++ marimo/spec/models/arc_spec.rb
@@ -11,3 +11,17 @@ describe Arc, "tags" do
     arcs(:one).tags.should include( tags(:two) )
   end
 end
+
+describe Arc, "search" do
+  fixtures :arcs
+
+  it "text TA0013 で検索すると two にマッチする" do
+    @search = Arc::Search.new('text' => 'TA0013')
+    Arc.find(:all, @search.find_options).should == [ arcs(:two) ]
+  end
+
+  it "text tattoo で検索すると two にマッチする" do
+    @search = Arc::Search.new('text' => 'tattoo')
+    Arc.find(:all, @search.find_options).should == [ arcs(:two) ]
+  end
+end
