3f0008d9e2dc7100b9a6dc3f44695cc4f3cf7750
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-12 17:59:31 +0000

    merged trunk r159:166

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,13 +3,15 @@ class Relationship < ActiveRecord::Base
   belongs_to :to, :class_name => 'Arc'
 
   def validate_on_create
-    errors.add_to_base("self reference") if from_id == to_id
-    errors.add_to_base("this relationship has been taken") if Relationship.find(:first, :conditions => ["from_id = ? and to_id = ?", from.id, to.id])
-    errors.add_to_base("this relationship has been taken") if Relationship.find(:first, :conditions => ["from_id = ? and to_id = ?", to.id, from.id])
+    unless from.nil? || to.nil?
+      errors.add_to_base("self reference") if from_id == to_id
+      errors.add_to_base("this relationship has been taken") if Relationship.find(:first, :conditions => ["from_id = ? and to_id = ?", from.id, to.id])
+      errors.add_to_base("this relationship has been taken") if Relationship.find(:first, :conditions => ["from_id = ? and to_id = ?", to.id, from.id])
+    end
   end
 
   def self.kind_collection
-    [['同一内容', 1], ['新版', 2], ['前提', 3]]
+    [['同一内容', 1], ['新版', 2], ['前提', 3], ['旧版',-2], ['提供',-3]]
   end
 
   def kind_caption
@@ -20,18 +22,39 @@ class Relationship < ActiveRecord::Base
       '新版'
     when 3
       '前提'
+    when -2
+      '旧版'
+    when -3
+      '提供'
     end
+  end
+
+  def self.rev_kind_collection
+    [['同一内容', 1], ['新版',-2], ['前提',-3], ['旧版', 2], ['提供', 3]]
   end
 
   def rev_kind_caption
     case kind
     when 1
       '同一内容'
+    when -2
+      '新版'
+    when -3
+      '前提'
     when 2
       '旧版'
     when 3
       '提供'
     end
+  end
+
+  def from_code
+    from ? from.code : nil
+  end
+
+  def from_code=(from_code)
+    from = Arc.find_by_code(from_code)
+    self.from_id = from ? from.id : nil
   end
 
   def to_code
@@ -44,6 +67,15 @@ class Relationship < ActiveRecord::Base
   end
 
   def should_destroy?
-    to_id.nil?
+    from_id.nil? || to_id.nil?
+  end
+
+  def before_save
+    if kind < 0
+      self.kind = -kind
+      x_id = from_id
+      self.from_id = to_id
+      self.to_id = x_id
+    end
   end
 end
