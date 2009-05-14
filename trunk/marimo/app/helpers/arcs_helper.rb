1e4bc579b0d2ff96e4efc9091df35e39f2924014
rails <rails@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-14 18:12:17 +0000

    auto complete tag_name

--- trunk/marimo/app/helpers/arcs_helper.rb
+++ trunk/marimo/app/helpers/arcs_helper.rb
@@ -17,6 +17,15 @@ module ArcsHelper
     end
   end
 
+  def add_arc_tag_function
+  h <<'EOT'
+    var i = c1();
+    new Insertion.Bottom("arc_tags", "<p>\n  <label for=\"arc_tag_"+i+"\">É^ÉO</label><br />\n  <input id=\"arc_tag_"+i+"\" name=\"arc[arc_tag_attributes][][tag_name]\" size=\"30\" type=\"text\" />\n<div class=\"auto_complete\" id=\"arc_tag_ac_"+i+"\"></div>\n</p>\n");
+    new Ajax.Autocompleter('arc_tag_'+i, 'arc_tag_ac_'+i, '/arcs/auto_complete_for_tag_name', {frequency:1.0});
+    return false;
+EOT
+  end
+
   def relationship_kind_collection
     Relationship.kind_collection
   end
