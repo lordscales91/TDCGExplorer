50af8840be93d3e24bd45313ba03f83dfe96b78b
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-27 16:52:31 +0000

    tagging tsoview-011

---
+++ tags/tsoview-011/build.bat
@@ -0,0 +1,1 @@
+csc /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:Microsoft.DirectX.Direct3DX.dll,Microsoft.DirectX.Direct3D.dll,Microsoft.DirectX.dll /r:ICSharpCode.SharpZipLib.DLL TSOFile.cs TMOFile.cs TMOWriter.cs PNGFile.cs PNGWriter.cs Shader.cs TSOCamera.cs TSOView.cs
