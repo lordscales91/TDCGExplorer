53da3b3c7b6a5b5e424e1a174406714edb2908d9
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-05-29 12:08:00 +0000

    tagging tsoview-013

---
+++ tags/tsoview-013/TSOCamera.cs
@@ -0,0 +1,205 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+public class TSOCamera
+{
+    internal Vector3 center = Vector3.Empty;    //回転中心位置
+    internal Vector3 translation = Vector3.Empty;       //view座標上の位置
+    internal Vector3 camPosL = new Vector3(0.0f, 0.0f, -10.0f); //カメラ位置
+    internal Vector3 camDirDef = Vector3.Empty; //カメラ移動方向ベクトル
+    internal float offsetZ = 0.0f;      //カメラ奥行オフセット値
+    internal bool needUpdate = true;    //更新したか
+    internal Matrix viewMat = Matrix.Identity;  //ビュー行列
+    internal Matrix camPoseMat = Matrix.Identity;       //カメラ姿勢行列
+    internal float camZRotDef = 0.0f;   //カメラ Z軸回転差分
+    internal float camAngleUnit = 0.02f;        //移動時回転単位（ラジアン）
+
+    public Vector3 Center { get { return center; } set { center = value; } }
+    public Vector3 Translation { get { return translation; } set { translation = value; } }
+    public Vector3 CamPosL { get { return camPosL; } set { camPosL = value; } }
+    public Matrix CamPoseMat { get { return camPoseMat; } set { camPoseMat = value; } }
+
+    /// <summary>カメラ位置と姿勢を標準出力へ書き出す</summary>
+    public void Dump()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(TSOCamera));
+        XmlWriterSettings settings = new XmlWriterSettings();
+        settings.Encoding = Encoding.GetEncoding("Shift_JIS");
+        settings.Indent = true;
+        XmlWriter writer = XmlWriter.Create(Console.Out, settings);
+        serializer.Serialize(writer, this);
+        writer.Close();
+    }
+
+    /// <summary>カメラ位置と姿勢を書き出す</summary>
+    public void Save(string dest_file)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(TSOCamera));
+        XmlWriterSettings settings = new XmlWriterSettings();
+        settings.Encoding = Encoding.GetEncoding("Shift_JIS");
+        settings.Indent = true;
+        XmlWriter writer = XmlWriter.Create(dest_file, settings);
+        serializer.Serialize(writer, this);
+        writer.Close();
+    }
+
+    /// <summary>カメラ位置と姿勢を読み込む</summary>
+    public static TSOCamera Load(string source_file)
+    {
+        XmlReader reader = XmlReader.Create(source_file);
+        XmlSerializer serializer = new XmlSerializer(typeof(TSOCamera));
+        TSOCamera camera = serializer.Deserialize(reader) as TSOCamera;
+        reader.Close();
+        return camera;
+    }
+
+    public static TSOCamera Slerp(TSOCamera cam1, TSOCamera cam2, float ratio)
+    {
+        TSOCamera camera = new TSOCamera();
+        camera.Center = Vector3.Lerp(cam1.Center, cam2.Center, ratio);
+        camera.Translation = Vector3.Lerp(cam1.Translation, cam2.Translation, ratio);
+        camera.CamPosL = Vector3.Lerp(cam1.CamPosL, cam2.CamPosL, ratio);
+        Quaternion q1 = Quaternion.RotationMatrix(cam1.CamPoseMat);
+        Quaternion q2 = Quaternion.RotationMatrix(cam2.CamPoseMat);
+        camera.CamPoseMat = Matrix.RotationQuaternion(Quaternion.Slerp(q1, q2, ratio));
+        return camera;
+    }
+
+    /// <summary>カメラ位置と姿勢をリセット</summary>
+    public void Reset()
+    {
+        center = Vector3.Empty;
+        translation = Vector3.Empty;
+        camPosL = new Vector3(0.0f, 0.0f, -10.0f);
+        camPoseMat = Matrix.Identity;
+        needUpdate = true;
+    }
+
+    /// <summary>カメラ位置更新</summary>
+    /// <param name="camDirX">移動方向（経度）</param>
+    /// <param name="camDirY">移動方向（緯度）</param>
+    /// <param name="offsetZ">奥行オフセット値</param>
+    public void Move(float camDirX, float camDirY, float offsetZ)
+    {
+        if (camDirX == 0.0f && camDirY == 0.0f && offsetZ == 0.0f)
+            return;
+
+        camDirDef.X += camDirX;
+        camDirDef.Y += camDirY;
+        this.offsetZ += offsetZ;
+        needUpdate = true;
+    }
+
+    /// <summary>Z軸回転</summary>
+    public void RotZ(float radian)
+    {
+        if (radian == 0.0f)
+            return;
+
+        camZRotDef = radian;
+        needUpdate = true;
+    }
+
+    /// <summary>カメラ更新</summary>
+    public void Update()
+    {
+        if (! needUpdate)
+            return;
+
+        //カメラ Z軸回転で姿勢を仮更新
+        camPoseMat = Matrix.RotationZ(camZRotDef) * camPoseMat;
+
+        //緯度経度の差分移動
+        Vector3 dL = Vector3.TransformCoordinate(camDirDef, camPoseMat);
+        if (dL.X != 0.0f || dL.Y != 0.0f || dL.Z != 0.0f)
+        {
+            Vector3 camZAxis = new Vector3(camPoseMat.M31, camPoseMat.M32, camPoseMat.M33);
+            Vector3 rotAxis = Vector3.Cross(dL, camZAxis);
+            Quaternion q = Quaternion.RotationAxis(rotAxis, camAngleUnit * camDirDef.Length());
+            Matrix rotMat = Matrix.RotationQuaternion(q);
+            camPosL = Vector3.TransformCoordinate(camPosL, rotMat);
+
+            // 移動後カメラ姿勢を更新
+            Vector3 z = Vector3.Normalize(-camPosL);
+            Vector3 y = new Vector3(camPoseMat.M21, camPoseMat.M22, camPoseMat.M23);
+            Vector3 x = Vector3.Normalize(Vector3.Cross(y, z));
+            y = Vector3.Normalize(Vector3.Cross(z, x));
+            {
+                Matrix m = Matrix.Identity;
+                m.M11 = x.X;
+                m.M12 = x.Y;
+                m.M13 = x.Z;
+                m.M21 = y.X;
+                m.M22 = y.Y;
+                m.M23 = y.Z;
+                m.M31 = z.X;
+                m.M32 = z.Y;
+                m.M33 = z.Z;
+                camPoseMat = m;
+            }
+        }
+
+        // 奥行オフセットを更新
+        if (camPosL.Length() - offsetZ > 0)
+        {
+            Vector3 z = Vector3.Normalize(-camPosL);
+            camPosL += offsetZ * z;
+        }
+
+        //view行列更新
+        Vector3 posW = camPosL + center;
+        {
+            Matrix m = camPoseMat;
+            m.M41 = posW.X;
+            m.M42 = posW.Y;
+            m.M43 = posW.Z;
+            m.M44 = 1.0f;
+            viewMat = Matrix.Invert(m) * Matrix.Translation(-translation);
+        }
+
+        //差分をリセット
+        ResetDefValue();
+        needUpdate = false;
+    }
+
+    /// <summary>view行列を取得</summary>
+    public Matrix GetViewMatrix()
+    {
+        return viewMat;
+    }
+
+    /// <summary>回転中心位置を設定</summary>
+    public void SetCenter(Vector3 center)
+    {
+        this.center = center;
+        needUpdate = true;
+    }
+
+    /// <summary>view座標上の位置を設定</summary>
+    public void SetTranslation(Vector3 translation)
+    {
+        this.translation = translation;
+        needUpdate = true;
+    }
+
+    /// <summary>view座標上で移動</summary>
+    public void MoveView(float dx, float dy)
+    {
+        this.translation.X += dx;
+        this.translation.Y += dy;
+        needUpdate = true;
+    }
+
+    // 差分をリセット
+    protected void ResetDefValue()
+    {
+        camDirDef = Vector3.Empty;
+        offsetZ = 0.0f;
+        camZRotDef = 0.0f;
+    }
+}
---
+++ tags/tsoview-013/TSOFigureForm.cs
@@ -0,0 +1,194 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+//using System.ComponentModel;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TAHdecrypt
+{
+
+public class TSOFigureForm : Form
+{
+    Button btn1;
+    Button btnUp;
+    Button btnDown;
+    ListView lv_fig;
+    ListView lv;
+    DataGridView dg;
+
+    public TSOFigureForm()
+    {
+        this.ClientSize = new System.Drawing.Size(800, 600);
+        this.Text = "TSOGrid";
+        //this.AllowDrop = true;
+
+        btn1 = new Button();
+        btn1.Location = new Point(10, 10);
+        btn1.Text = "Dump";
+        btn1.Click += new EventHandler(btn1_Click);
+        this.Controls.Add(btn1);
+
+        btnUp = new Button();
+        btnUp.Location = new Point(95, 10);
+        btnUp.Text = "&Up";
+        btnUp.Click += new EventHandler(btnUp_Click);
+        this.Controls.Add(btnUp);
+
+        btnDown = new Button();
+        btnDown.Location = new Point(180, 10);
+        btnDown.Text = "&Down";
+        btnDown.Click += new EventHandler(btnDown_Click);
+        this.Controls.Add(btnDown);
+
+        lv_fig = new ListView();
+        lv_fig.Bounds = new Rectangle(new Point(10, 40), new Size(100, 200));
+        lv_fig.View = View.Details;
+        lv_fig.FullRowSelect = true;
+        lv_fig.HideSelection = false;
+        lv_fig.MultiSelect = false;
+        lv_fig.GridLines = true;
+
+        lv_fig.Columns.Add("Name", -2, HorizontalAlignment.Left);
+        lv_fig.SelectedIndexChanged += lv_fig_SelectedIndexChanged;
+
+        this.Controls.Add(lv_fig);
+
+        lv = new ListView();
+        lv.Bounds = new Rectangle(new Point(120, 40), new Size(300, 200));
+        lv.View = View.Details;
+        lv.FullRowSelect = true;
+        lv.HideSelection = false;
+        lv.MultiSelect = false;
+        lv.GridLines = true;
+
+        lv.Columns.Add("Name", -2, HorizontalAlignment.Left);
+        lv.Columns.Add("File", -2, HorizontalAlignment.Left);
+        lv.SelectedIndexChanged += lv_SelectedIndexChanged;
+
+        this.Controls.Add(lv);
+
+        dg = new DataGridView();
+        dg.Bounds = new Rectangle(new Point(10, 250), new Size(410, 250));
+        dg.EditMode = DataGridViewEditMode.EditOnEnter;
+        dg.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        this.Controls.Add(dg);
+    }
+
+    protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+    {
+        //this.Render(); // Render on painting
+    }
+
+    protected override void OnKeyPress(System.Windows.Forms.KeyPressEventArgs e)
+    {
+        if ((int)(byte)e.KeyChar == (int)System.Windows.Forms.Keys.Escape)
+            this.Dispose(); // Esc was pressed
+    }
+
+    private TSOFigure fig = null;
+    private TSOFile tso = null;
+    private Shader shader = null;
+
+    public void Clear()
+    {
+        dg.DataSource = null;
+        this.shader = null;
+        lv.Items.Clear();
+        this.tso = null;
+        lv_fig.Items.Clear();
+        this.fig = null;
+    }
+
+    public void SetTSOFigure(TSOFigure fig)
+    {
+        this.fig = fig;
+        lv_fig.Items.Clear();
+        for (int i = 0; i < fig.TSOList.Count; i++)
+        {
+            TSOFile tso = fig.TSOList[i];
+            ListViewItem li = new ListViewItem("TSO #" + i.ToString());
+            li.Tag = tso;
+            lv_fig.Items.Add(li);
+        }
+        lv_fig.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+    }
+
+    public void SetTSOFile(TSOFile tso)
+    {
+        this.tso = tso;
+        lv.Items.Clear();
+        foreach (TSOSubScript sub_script in tso.sub_scripts)
+        {
+            ListViewItem li = new ListViewItem(sub_script.Name);
+            li.SubItems.Add(sub_script.File);
+            li.Tag = sub_script;
+            lv.Items.Add(li);
+        }
+        lv.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+    }
+
+    public void SetShader(Shader shader)
+    {
+        this.shader = shader;
+        dg.DataSource = shader.shader_parameters;
+    }
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        if (shader == null)
+            return;
+        Console.WriteLine("-- dump shader parameters --");
+        foreach (ShaderParameter param in shader.shader_parameters)
+            Console.WriteLine("Name {0} F1 {1} F2 {2} F3 {3} F4 {4}", param.Name, param.F1, param.F2, param.F3, param.F4);
+    }
+
+    protected void btnUp_Click(object sender, EventArgs e)
+    {
+        if (lv_fig.SelectedItems.Count == 0)
+            return;
+        int li_idx = lv_fig.SelectedIndices[0];
+        int li_idx_prev = li_idx-1;
+        if (li_idx_prev < 0)
+            return;
+        fig.SwapAt(li_idx_prev, li_idx);
+        SetTSOFigure(fig);
+        ListViewItem li = lv_fig.Items[li_idx_prev];
+        li.Selected = true;
+    }
+
+    protected void btnDown_Click(object sender, EventArgs e)
+    {
+        if (lv_fig.SelectedItems.Count == 0)
+            return;
+        int li_idx = lv_fig.SelectedIndices[0];
+        int li_idx_next = li_idx+1;
+        if (li_idx_next > lv_fig.Items.Count-1)
+            return;
+        fig.SwapAt(li_idx, li_idx_next);
+        SetTSOFigure(fig);
+        ListViewItem li = lv_fig.Items[li_idx_next];
+        li.Selected = true;
+    }
+
+    protected void lv_fig_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (lv_fig.SelectedItems.Count == 0)
+            return;
+        ListViewItem li = lv_fig.SelectedItems[0];
+        TSOFile tso = li.Tag as TSOFile;
+        SetTSOFile(tso);
+    }
+
+    protected void lv_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (lv.SelectedItems.Count == 0)
+            return;
+        ListViewItem li = lv.SelectedItems[0];
+        TSOSubScript sub_script = li.Tag as TSOSubScript;
+        SetShader(sub_script.shader);
+    }
+}
+}
