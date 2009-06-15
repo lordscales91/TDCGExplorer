c7a5f3e63eb18d9d71fd490905ecb30e8a59c734
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-15 12:21:37 +0000

    col bases

--- marimo/spec/models/tso_spec.rb
+++ marimo/spec/models/tso_spec.rb
@@ -1,5 +1,52 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+describe Tso, "row" do
+  fixtures :tsos
+
+  it "one" do
+    tsos(:one).row.should == 'A'
+  end
+
+  it "”wŒi‚È‚ç nil ‚ð“¾‚é" do
+    tso = Tso.new(:path => 'data/bgmodel/HOTEL_C.tso')
+    tso.row.should be_nil
+  end
+end
+
+describe Tso, "col_base_path" do
+  fixtures :tsos
+
+  it "one" do
+    tsos(:one).col_base_path.should == 'data/model/N400BODY_A00.tso'
+  end
+
+  it "two" do
+    tsos(:two).col_base_path.should == 'data/model/N400BODY_A00.tso'
+  end
+
+  it "”wŒi‚È‚ç nil ‚ð“¾‚é" do
+    tso = Tso.new(:path => 'data/bgmodel/HOTEL_C.tso')
+    tso.col_base_path.should be_nil
+  end
+end
+
+describe Tso, "col_bases" do
+  fixtures :tsos
+
+  it "one" do
+    tsos(:one).col_bases.should be_empty
+  end
+
+  it "two" do
+    tsos(:two).col_bases.should == [ tsos(:one) ]
+  end
+
+  it "”wŒi‚È‚ç nil ‚ð“¾‚é" do
+    tso = Tso.new(:path => 'data/bgmodel/HOTEL_C.tso')
+    tso.col_bases.should be_empty
+  end
+end
+
 describe Tso, "search" do
   fixtures :tsos
 
