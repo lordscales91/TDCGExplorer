7950ddf77c879fe5252433bc76dad064d2f4e2b0
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-17 14:17:57 +0000

    merged trunk r265:274

--- branches/marimo-rc1/spec/models/tah_spec.rb
+++ branches/marimo-rc1/spec/models/tah_spec.rb
@@ -1,5 +1,34 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+describe Tah, "col_bases" do
+  fixtures :tahs, :tsos, :tso_col_bases
+
+  it "one" do
+    tahs(:one).col_bases.should be_empty
+  end
+
+  it "two" do
+    tahs(:two).col_bases.should == [ tahs(:one) ]
+  end
+
+  describe "tsos(:two).tah == tahs(:one) ‚È‚ç" do
+    before do
+      TAHHash.should_receive(:calc).with('data/model/N400BODY_A00.tso').and_return(0xBC0EEF52) # in update_col_base
+      TAHHash.should_receive(:calc).with('data/model/N400BODY_A01.tso').and_return(0xBC0EFF52) # in before_save
+      tsos(:two).tah_id = 1
+      tsos(:two).save!
+    end
+
+    it "one: self ‚ÍŠÜ‚Ü‚È‚¢" do
+      tahs(:one).col_bases.should == [ ]
+    end
+
+    it "two" do
+      tahs(:two).col_bases.should == [ ]
+    end
+  end
+end
+
 describe Tah, "search" do
   fixtures :tahs
 
