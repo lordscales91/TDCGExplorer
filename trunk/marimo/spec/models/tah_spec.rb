e3ba7744cc464bf958eebef168b3390dc98c9d70
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-17 14:00:32 +0000

    added spec

--- trunk/marimo/spec/models/tah_spec.rb
+++ trunk/marimo/spec/models/tah_spec.rb
@@ -10,6 +10,23 @@ describe Tah, "col_bases" do
   it "two" do
     tahs(:two).col_bases.should == [ tahs(:one) ]
   end
+
+  describe "tsos(:two).tah == tahs(:one) ‚È‚ç" do
+    before do
+      TAHHash.should_receive(:calc).with('data/model/N400BODY_A00.tso').and_return(0xBC0EEF52) # in update_col_base
+      TAHHash.should_receive(:calc).with('data/model/N400BODY_A01.tso').and_return(0xBC0EFF52) # in before_save
+      tsos(:two).tah_id = 1
+      tsos(:two).save!
+    end
+
+    it "one" do
+      tahs(:one).col_bases.should == [ tahs(:one) ]
+    end
+
+    it "two" do
+      tahs(:two).col_bases.should == [ ]
+    end
+  end
 end
 
 describe Tah, "search" do
