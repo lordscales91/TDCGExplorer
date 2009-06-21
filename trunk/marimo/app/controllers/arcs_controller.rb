dc99ce6ea7bbd061c708b4af8699d4a554d9ebee
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-21 19:29:09 +0000

    arcs code_revs

--- trunk/marimo/app/controllers/arcs_controller.rb
+++ trunk/marimo/app/controllers/arcs_controller.rb
@@ -56,6 +56,18 @@ class ArcsController < ApplicationController
     end
   end
 
+  # GET /arcs/code/TA0001/revs
+  # GET /arcs/code/TA0001/revs.xml
+  def code_revs
+    @arc = Arc.find_by_code(params[:code])
+    @arc_revs = @arc.rev_relationships
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @arc_revs.to_xml(:except => [ :created_at, :updated_at ]) }
+    end
+  end
+
   # GET /arcs/new
   # GET /arcs/new.xml
   def new
