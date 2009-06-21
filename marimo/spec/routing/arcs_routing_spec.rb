d30c8c80a54e6f478141326c20a9f1b01695946c
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-21 18:45:51 +0000

    added arcs routing spec

---
+++ marimo/spec/routing/arcs_routing_spec.rb
@@ -0,0 +1,23 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ArcsController do
+  describe "route generation" do
+    it "maps #index" do
+      route_for(:controller => "arcs", :action => "index").should == "/arcs"
+    end
+  
+    it "maps #show" do
+      route_for(:controller => "arcs", :action => "show", :id => "1").should == "/arcs/1"
+    end
+  end
+
+  describe "route recognition" do
+    it "generates params for #index" do
+      params_from(:get, "/arcs").should == {:controller => "arcs", :action => "index"}
+    end
+  
+    it "generates params for #show" do
+      params_from(:get, "/arcs/1").should == {:controller => "arcs", :action => "show", :id => "1"}
+    end
+  end
+end
