470cc492be4f0154f8480cb6bd5873ad29e73695
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-22 05:44:47 +0000

    force platform x86

--- tahtool/build-TAHTool.bat
+++ tahtool/build-TAHTool.bat
@@ -1,3 +1,3 @@
-csc /unsafe /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:Microsoft.DirectX.Direct3D.dll,Microsoft.DirectX.dll TMOFile.cs TMOWriter.cs Boin.cs TAHBoin.cs
-csc /unsafe /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:Microsoft.DirectX.Direct3D.dll,Microsoft.DirectX.dll TMOFile.cs TMOWriter.cs Dom.cs TAHDom.cs
-csc /unsafe /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:Microsoft.DirectX.Direct3D.dll,Microsoft.DirectX.dll TMOFile.cs TMOWriter.cs Pedo.cs TAHPedo.cs
+csc /platform:x86 /unsafe /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:Microsoft.DirectX.Direct3D.dll,Microsoft.DirectX.dll TMOFile.cs TMOWriter.cs Boin.cs TAHBoin.cs
+csc /platform:x86 /unsafe /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:Microsoft.DirectX.Direct3D.dll,Microsoft.DirectX.dll TMOFile.cs TMOWriter.cs Dom.cs TAHDom.cs
+csc /platform:x86 /unsafe /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:Microsoft.DirectX.Direct3D.dll,Microsoft.DirectX.dll TMOFile.cs TMOWriter.cs Pedo.cs TAHPedo.cs
