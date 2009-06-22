eff4555413f44ba33ba3c7e33bf57aeb49f3e4d8
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-22 05:26:02 +0000

    imported

---
+++ trunk/tahtool/patch-TAHTool.bat
@@ -0,0 +1,11 @@
+copy vendor\TAHdecrypt\Program.cs Program.cs
+patch < TAHBoin.patch
+move Program.cs TAHBoin.cs
+
+copy vendor\TAHdecrypt\Program.cs Program.cs
+patch < TAHPedo.patch
+move Program.cs TAHPedo.cs
+
+copy vendor\TAHdecrypt\Program.cs Program.cs
+patch < TAHDom.patch
+move Program.cs TAHDom.cs
