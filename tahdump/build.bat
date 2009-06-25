1feb8ba6a94deb883ccf352f53598596abeff608
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-25 08:09:08 +0000

    imported

---
+++ tahdump/build.bat
@@ -0,0 +1,1 @@
+csc /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:ArchiveLib.dll /r:ICSharpCode.SharpZipLib.DLL MersenneTwister.cs LZSS.cs TAHCryptStream.cs TAHUtil.cs TAHFile.cs TAHDump.cs
