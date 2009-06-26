db36e0fac52cacc7b4e1a1dc3a94dd4edf70eef0
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-26 15:58:28 +0000

    added tmotool.rsp

---
+++ trunk/tmotool/build.bat
@@ -0,0 +1,11 @@
+csc @tmotool.rsp Move.cs TMOMove.cs
+csc @tmotool.rsp RotY.cs TMORotY.cs
+csc @tmotool.rsp Zoom.cs TMOZoom.cs
+csc @tmotool.rsp Boin.cs TMOBoin.cs
+csc @tmotool.rsp Dom.cs TMODom.cs
+csc @tmotool.rsp Pedo.cs TMOPedo.cs
+csc @tmotool.rsp TMONodeCopy.cs
+csc @tmotool.rsp TMONodeDump.cs
+csc @tmotool.rsp TMOPose.cs
+csc @tmotool.rsp TMOAppend.cs
+csc @tmotool.rsp TMOMotionCopy.cs
