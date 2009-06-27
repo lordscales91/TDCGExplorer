94b859e087e9c938c8307df626040ce15b63bd57
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-27 02:28:40 +0000

    command on TMODom

--- tmotool/build.bat
+++ tmotool/build.bat
@@ -2,7 +2,7 @@ csc @tmotool.rsp Move.cs TMOMove.cs
 csc @tmotool.rsp RotY.cs TMORotY.cs
 csc @tmotool.rsp Zoom.cs TMOZoom.cs
 csc @tmotool.rsp command\Boin.cs TMOBoin.cs
-csc @tmotool.rsp Dom.cs TMODom.cs
+csc @tmotool.rsp command\Dom.cs TMODom.cs
 csc @tmotool.rsp Pedo.cs TMOPedo.cs
 csc @tmotool.rsp TMONodeCopy.cs
 csc @tmotool.rsp TMONodeDump.cs
