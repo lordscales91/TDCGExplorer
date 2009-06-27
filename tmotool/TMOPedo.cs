35c18910a0803ad80de806a11b6b3b7031344bd9
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-27 02:30:51 +0000

    command on TMOPedo

--- tmotool/TMOPedo.cs
+++ tmotool/TMOPedo.cs
@@ -1,8 +1,15 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace TAHdecrypt
 {
+    public interface ITMOCommand
+    {
+        Dictionary<string, TMONode> Nodes { set; }
+        void Execute();
+    }
+
     class TMOPedo
     {
         static void Main(string[] args) 
@@ -14,8 +21,48 @@ namespace TAHdecrypt
             }
 
             string source_file = args[0];
+            string dest_file = source_file + ".tmp";
 
-            Pedo.UpdateTmo(source_file);
+            TMOFile tmo = new TMOFile();
+            try
+            {
+                tmo.Load(source_file);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            if (tmo.nodes[0].ShortName != "W_Hips") {
+                Console.WriteLine("Passed: root node is not W_Hips");
+                return;
+            }
+
+            Dictionary<string, TMONode> nodes = new Dictionary<string, TMONode>();
+
+            foreach(TMONode node in tmo.nodes)
+            try {
+                nodes.Add(node.ShortName, node);
+            } catch (ArgumentException) {
+                Console.WriteLine("node {0} already exists.", node.ShortName);
+            }
+
+            try {
+                ITMOCommand command = new TDCG.TMOTool.Command.Pedo();
+                command.Nodes = nodes;
+                command.Execute();
+            } catch (KeyNotFoundException) {
+                Console.WriteLine("node not found.");
+            }
+
+            tmo.Save(dest_file);
+
+            System.IO.File.Delete(source_file);
+            System.IO.File.Move(dest_file, source_file);
+            Console.WriteLine("updated " + source_file);
+
+            return;
         }
     }
 }
