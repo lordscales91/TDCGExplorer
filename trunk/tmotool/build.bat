927ab7b9709df0fba47e0517880b52001a62e5e5
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-27 02:30:51 +0000

    command on TMOPedo

--- trunk/tmotool/build.bat
+++ trunk/tmotool/build.bat
@@ -3,7 +3,7 @@ csc @tmotool.rsp RotY.cs TMORotY.cs
 csc @tmotool.rsp Zoom.cs TMOZoom.cs
 csc @tmotool.rsp command\Boin.cs TMOBoin.cs
 csc @tmotool.rsp command\Dom.cs TMODom.cs
-csc @tmotool.rsp Pedo.cs TMOPedo.cs
+csc @tmotool.rsp command\Pedo.cs TMOPedo.cs
 csc @tmotool.rsp TMONodeCopy.cs
 csc @tmotool.rsp TMONodeDump.cs
 csc @tmotool.rsp TMOPose.cs
