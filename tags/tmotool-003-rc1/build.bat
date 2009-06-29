878ebf76f9677d8c72f20b8771b1e7602d09b4f8
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-29 09:54:53 +0000

    tagging tmotool 003 RC1

---
+++ tags/tmotool-003-rc1/build.bat
@@ -0,0 +1,13 @@
+csc @tmotool.rsp Move.cs TMOMove.cs
+csc @tmotool.rsp RotY.cs TMORotY.cs
+csc @tmotool.rsp Zoom.cs TMOZoom.cs
+csc @tmotool.rsp command\Boin.cs TMOBoin.cs
+csc @tmotool.rsp command\Dom.cs TMODom.cs
+csc @tmotool.rsp command\Pedo.cs TMOPedo.cs
+csc @tmotool.rsp TMONodeCopy.cs
+csc @tmotool.rsp TMONodeDump.cs
+csc @tmotool.rsp TMOPose.cs
+csc @tmotool.rsp TMOAppend.cs
+csc @tmotool.rsp TMOMotionCopy.cs
+csc @tmotool.rsp TMOAnim.cs
+csc @tmotool.rsp /r:CSScriptLibrary.dll TMOTool.cs
