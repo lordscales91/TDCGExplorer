f62324c0ece2f56b8568a19b44c5602f03a8059b
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-29 09:54:53 +0000

    tagging tmotool 003 RC1

--- marimo/db/migrate/20090512184837_add_index_location_and_code_on_arc.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexLocationAndCodeOnArc < ActiveRecord::Migration
-  def self.up
-    add_index :arcs, [:location, :code], :unique => true
-  end
-
-  def self.down
-    remove_index :arcs, :column => [:location, :code]
-  end
-end
