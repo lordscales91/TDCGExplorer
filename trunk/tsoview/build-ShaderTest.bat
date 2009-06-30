7e7d3a88de84f301298dd33e6fcadd9532e94dd5
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-30 17:50:33 +0000

    out build dir

--- trunk/tsoview/build-ShaderTest.bat
+++ trunk/tsoview/build-ShaderTest.bat
@@ -1,1 +1,1 @@
-csc /out:ShaderTest.exe @direct3d.rsp ShaderTest\Program.cs
+csc /out:build\ShaderTest.exe @direct3d.rsp ShaderTest\Program.cs
--- trunk/tsoview/build-TSOConfigDump.bat
+++ trunk/tsoview/build-TSOConfigDump.bat
@@ -1,1 +1,1 @@
-csc /out:TSOConfigDump.exe TSOConfig.cs TSOConfigDump\Program.cs
+csc /out:build\TSOConfigDump.exe TSOConfig.cs TSOConfigDump\Program.cs
