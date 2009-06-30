0be2f5047e5e32d03f906ba6720f1ff590faa3be
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-06-30 08:53:42 +0000

    ARCG.Program

--- trunk/tsoview/build-ARCG.bat
+++ trunk/tsoview/build-ARCG.bat
@@ -1,1 +1,1 @@
-csc @tsoview.rsp /r:NyARToolkitCSUtils.dll,NyARToolkitCS.dll,DirectShowLib-2005.dll ARCG.cs
+csc /out:ARCG.exe @tsoview.rsp /r:NyARToolkitCSUtils.dll,NyARToolkitCS.dll,DirectShowLib-2005.dll ARCG\Program.cs
