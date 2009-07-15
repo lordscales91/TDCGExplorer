99b0d2d15bf256d50faa5a6fb55fb78b742b225a
konoa <konoa@72ae2088-5f33-de11-a17b-0000e250a282>
2009-07-15 11:36:27 +0000

    0715b

---
+++ trunk/TDCGExplorer/TDCGExplorer/ReplaceDialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TDCGExplorer
+{
+    public partial class ReplaceDialog : Form
+    {
+        public ReplaceDialog()
+        {
+            InitializeComponent();
+        }
+
+        public string textTo
+        {
+            get { return textBoxToString.Text; }
+            set { textBoxToString.Text = value; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textTo.Length > 0)
+                DialogResult = DialogResult.OK;
+            else
+                DialogResult = DialogResult.None;
+        }
+    }
+}
