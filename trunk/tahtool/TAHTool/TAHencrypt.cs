632fd6828196ef09fe942fbc066216d186f14ad0
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-07-20 15:42:17 +0000

    refactored

--- trunk/tahtool/TAHTool/TAHencrypt.cs
+++ trunk/tahtool/TAHTool/TAHencrypt.cs
@@ -13,10 +13,53 @@ using System.IO;
                 if (Directory.Exists(source_file))
                 {
                     //launch encrypt routine from here...
-                    Encrypter encrypter = new Encrypter();
-                    encrypter.encrypt_archive(source_file + ".tah", source_file);
+                    encrypt_archive(source_file + ".tah", source_file);
                 }
             }
+        }
+
+        static int encrypt_archive(string file_path_name, string source_path)
+        {
+            //check if file already exists... if yes rename it
+            try
+            {
+                if (File.Exists(file_path_name))
+                {
+                    File.Move(file_path_name, file_path_name + ".bak");
+                }
+            }
+            catch (IOException)
+            {
+                System.Console.Out.WriteLine("Error: Could not rename existing TAH file.");
+                return -1;
+            }
+            Encrypter encrypter = new Encrypter();
+
+            //read in files from source path, do not compress them now.
+            //全ディレクトリ名
+            string[] directories = Directory.GetDirectories(source_path, "*", SearchOption.AllDirectories);
+
+            {
+                string dirname = source_path;
+                string[] files = Directory.GetFiles(dirname);
+                foreach (string file in files)
+                    encrypter.Add(file);
+            }
+            for (int i = 0; i < directories.Length; i++)
+            {
+                string dirname = directories[i];
+                string[] files = Directory.GetFiles(dirname);
+                foreach (string file in files)
+                    encrypter.Add(file);
+            }
+
+            encrypter.SourcePath = source_path;
+            encrypter.GetFileEntryStream = delegate(string true_file_name)
+            {
+                return File.OpenRead(true_file_name);
+            };
+            encrypter.Save(file_path_name);
+            return 0;
         }
 
     }
