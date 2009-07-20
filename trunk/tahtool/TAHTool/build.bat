f53f3a1006bad21b9df078cc0e6f1013e4a80578
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-07-20 13:55:04 +0000

    split compression

--- trunk/tahtool/TAHTool/build.bat
+++ trunk/tahtool/TAHTool/build.bat
@@ -1,2 +1,2 @@
-csc /debug /out:..\build\TAHdecrypt.exe MT19937ar.cs Decrypter.cs TAHdecrypt.cs
-csc /debug /out:..\build\TAHencrypt.exe MT19937ar.cs Encrypter.cs TAHencrypt.cs
+csc /debug /out:..\build\TAHdecrypt.exe MT19937ar.cs Decompression.cs Decrypter.cs TAHdecrypt.cs
+csc /debug /out:..\build\TAHencrypt.exe MT19937ar.cs Compression.cs Encrypter.cs TAHencrypt.cs
