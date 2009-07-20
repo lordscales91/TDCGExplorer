d11f6c0804e7162348fc295839f770c238629063
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-07-20 10:08:30 +0000

    removed Copy from Decrypter

--- trunk/tahtool/TAHTool/Decrypter.cs
+++ trunk/tahtool/TAHTool/Decrypter.cs
@@ -70,51 +70,6 @@ using System.IO;
             public UInt32 offset;
         }
 
-        static unsafe void Copy(byte[] src, int srcIndex,
-            byte[] dst, int dstIndex, int count)
-        {
-            if (src == null || srcIndex < 0 ||
-                dst == null || dstIndex < 0 || count < 0)
-            {
-                throw new ArgumentException();
-            }
-            int srcLen = src.Length;
-            int dstLen = dst.Length;
-            if (srcLen - srcIndex < count ||
-                dstLen - dstIndex < count)
-            {
-                throw new ArgumentException();
-            }
-
-
-            // The following fixed statement pins the location of
-            // the src and dst objects in memory so that they will
-            // not be moved by garbage collection.          
-            fixed (byte* pSrc = src, pDst = dst)
-            {
-                byte* ps = pSrc + srcIndex;
-                byte* pd = pDst + dstIndex;
-
-                // Loop over the count in blocks of 4 bytes, copying an
-                // integer (4 bytes) at a time:
-                for (int n = 0; n < count / 4; n++)
-                {
-                    *((int*)pd) = *((int*)ps);
-                    pd += 4;
-                    ps += 4;
-                }
-
-                // Complete the copy by moving any bytes that weren't
-                // moved in blocks of 4:
-                for (int n = 0; n < count % 4; n++)
-                {
-                    *pd = *ps;
-                    pd++;
-                    ps++;
-                }
-            }
-        }
-
         public int decrypt_archive(string source_file, string dest_path)
         {
             directory_meta_info directory_meta_infos = new directory_meta_info();
---
+++ trunk/tahtool/TAHTool/build.bat
@@ -0,0 +1,2 @@
+csc /debug /out:..\build\TAHdecrypt.exe MT19937ar.cs Decrypter.cs TAHdecrypt.cs
+csc /debug /out:..\build\TAHencrypt.exe /unsafe MT19937ar.cs Encrypter.cs TAHencrypt.cs
