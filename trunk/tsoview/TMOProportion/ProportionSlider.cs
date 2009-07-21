b49ab1c4345ef54cd60150d959ca64bc5b525690
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-07-21 07:21:02 +0000

    slider.Ratio

--- trunk/tsoview/TMOProportion/ProportionSlider.cs
+++ trunk/tsoview/TMOProportion/ProportionSlider.cs
@@ -11,9 +11,37 @@ namespace TMOProportion
 {
     public partial class ProportionSlider : UserControl
     {
+        public event EventHandler ValueChanged;
+
+        float ratio = 0.0f;
+        public float Ratio
+        {
+            get
+            {
+                return ratio;
+            }
+            set
+            {
+                if (ratio != value)
+                {
+                    ratio = value;
+                    int ratioTrackBarValue = (int)(ratio * 10.0f);
+                    trackBar.Value = ratioTrackBarValue;
+                    lbRatio.Text = string.Format("{0:F2}", ratio);
+                    if (ValueChanged != null)
+                        ValueChanged(this, new EventArgs());
+                }
+            }
+
+        }
         public ProportionSlider()
         {
             InitializeComponent();
+        }
+
+        private void trackBar_ValueChanged(object sender, EventArgs e)
+        {
+            Ratio = trackBar.Value * 0.1f;
         }
     }
 }
