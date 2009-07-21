7dc3ca0060fc9ee0026dafc2f83492d6c2cb0f3b
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-07-21 04:05:18 +0000

    moved build.bat

--- trunk/tahtool/TAHTool/build.bat
+++
@@ -1,2 +0,0 @@
-csc /debug /out:..\build\TAHdecrypt.exe MT19937ar.cs Decompression.cs Decrypter.cs TAHdecrypt.cs
-csc /debug /out:..\build\TAHencrypt.exe MT19937ar.cs Compression.cs Encrypter.cs TAHencrypt.cs
