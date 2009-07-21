88e4b45d3cfda24c784644f39db4ba54cea9f808
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-07-21 03:39:41 +0000

    revised

--- trunk/tahtool/TAHTool/TAHdecrypt.cs
+++ trunk/tahtool/TAHTool/TAHdecrypt.cs
@@ -9,7 +9,6 @@ using System.IO;
             if (args.Length > 0)
             {
                 DumpFiles(args[0]);
-                //decrypt_TAH_archive(args[0]);
             }
         }
 
@@ -22,17 +21,17 @@ using System.IO;
 
             Console.WriteLine("file_name\toffset\tlength\tflag");
             decrypter.extract_TAH_directory();
-            foreach (TAHEntry info in decrypter.Entries)
+            foreach (TAHEntry entry in decrypter.Entries)
             {
-                Console.WriteLine("{0}\t{1}\t{2}\t{3}", info.file_name, info.offset, info.length, info.flag);
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}", entry.file_name, entry.offset, entry.length, entry.flag);
 
                 byte[] data_output;
-                decrypter.ExtractResource(info, out data_output);
+                decrypter.ExtractResource(entry, out data_output);
 
-                string file_name = info.file_name;
+                string file_name = entry.file_name;
 
                 //flag & 0x1 = 1‚È‚çno path
-                if (info.flag % 2 == 1)
+                if (entry.flag % 2 == 1)
                 {
                     string ext;
                     string magic = System.Text.Encoding.ASCII.GetString(data_output, 0, 4);
@@ -70,32 +69,4 @@ using System.IO;
 
             return 0;
         }
-
-        static int decrypt_TAH_archive(string source_file)
-        {
-            string dest_path = "";
-            string[] sep = new string[1];
-            sep[0] = "\\";
-            string[] file_path = source_file.Split(sep, System.StringSplitOptions.RemoveEmptyEntries);
-            string file_name = file_path[file_path.Length - 1];
-            if (file_name.Substring(file_name.Length - 3, 3).ToLower().CompareTo("tah") == 0)
-            {
-                //decrypt TAH archive
-                string folder_name = file_name.Substring(0, file_name.LastIndexOf("."));
-                for (int i = 0; i < file_path.Length - 1; i++)
-                {
-                    dest_path += file_path[i] + "\\";
-                }
-                dest_path += folder_name;
-                System.Console.Out.WriteLine(dest_path);
-                Decrypter decrypter = new Decrypter();
-                return decrypter.decrypt_archive(source_file, dest_path);
-            }
-            else
-            {
-                //encrypt to TAH archive
-                return -1;
-            }
-        }
-
     }
