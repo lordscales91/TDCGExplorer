663f0fdff3188514abfd1ae071401bd63949cdb4
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2009-07-26 17:09:12 +0000

    draw ground

--- trunk/tsoview/ShadowTest/Program.cs
+++ trunk/tsoview/ShadowTest/Program.cs
@@ -84,6 +84,7 @@ public class Viewer : IDisposable
     Matrix g_matL;
     Matrix g_matV;
     Matrix g_matP;
+    VertexBuffer g_pVBuffer;
 
     // マウスポイントしているスクリーン座標
     private Point lastScreenPoint = Point.Empty;
@@ -182,16 +183,64 @@ public class Viewer : IDisposable
                 return false;
             }
         }
-        effect.Technique = "Tec0_NormalDraw";
 
         m_fCamX = 30.0f;
         m_fCamY = 0.0f;
         m_fLitX = -60.0f;
         m_fLitY = 135.0f;
 
+        CustomVertex.PositionNormal[] verts = new CustomVertex.PositionNormal[4];
+        verts[0] = new CustomVertex.PositionNormal(-30, -5, +30, 0, 1, 0);
+        verts[1] = new CustomVertex.PositionNormal(+30, -5, +30, 0, 1, 0);
+        verts[2] = new CustomVertex.PositionNormal(-30, -5, -30, 0, 1, 0);
+        verts[3] = new CustomVertex.PositionNormal(+30, -5, -30, 0, 1, 0);
+
+        g_pVBuffer = new VertexBuffer(typeof(CustomVertex.PositionNormal), 4, device, Usage.Dynamic, CustomVertex.PositionNormal.Format, Pool.Default);
+        g_pVBuffer.SetData(verts, 0, LockFlags.None);
+
         teapot = Mesh.Teapot(device);
 
         return true;
+    }
+
+    private void CancelResize(object sender, CancelEventArgs e)
+    {
+        Console.WriteLine("CancelResize");
+        e.Cancel = true;
+    }
+
+    public void Render()
+    {
+        SParam sParam;
+        sParam.vCamAngle = new Vector3( Geometry.DegreeToRadian(m_fCamX), Geometry.DegreeToRadian(m_fCamY), 0.0f);
+        sParam.vLightAngle = new Vector3( Geometry.DegreeToRadian(m_fLitX), Geometry.DegreeToRadian(m_fLitY), 0.0f);
+        sParam.fCamLength = 80.0f;
+
+        g_matW = Matrix.Scaling(5,5,5);
+        g_matL = Matrix.Translation(15,0,0);
+
+        Vector3 vEye = new Vector3(0,0,-sParam.fCamLength);
+        Vector3 vCenter = new Vector3(0,0,0);
+        Vector3 vUp = new Vector3(0,1,0);
+        g_matV = Matrix.RotationYawPitchRoll(sParam.vCamAngle.Y, sParam.vCamAngle.X, 0.0f);
+        vEye = Vector3.TransformCoordinate(vEye, g_matV);
+        g_matV = Matrix.LookAtLH(vEye, vCenter, vUp);
+
+        g_matP = Matrix.PerspectiveFovLH(Geometry.DegreeToRadian(45.0f), 4.0f/3.0f, 1.0f, 200.0f);
+
+        device.BeginScene();
+
+        device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.CornflowerBlue, 1.0f, 0);
+
+        CalcLightTrans(ref sParam);
+        //DrawShadowMap(ref sParam);
+        //DrawGauss();
+        DrawReceiver();
+
+        device.EndScene();
+
+        device.Present();
+        Thread.Sleep(30);
     }
 
     void CalcLightTrans(ref SParam sParam)
@@ -214,58 +263,74 @@ public class Viewer : IDisposable
         }
     }
 
-    private void CancelResize(object sender, CancelEventArgs e)
+    void DrawShadowMap(ref SParam sParam)
     {
-        Console.WriteLine("CancelResize");
-        e.Cancel = true;
-    }
-
-    public void Render()
-    {
-        SParam sParam;
-        sParam.vCamAngle = new Vector3( Geometry.DegreeToRadian(m_fCamX), Geometry.DegreeToRadian(m_fCamY), 0.0f);
-        sParam.vLightAngle = new Vector3( Geometry.DegreeToRadian(m_fLitX), Geometry.DegreeToRadian(m_fLitY), 0.0f);
-        sParam.fCamLength = 80.0f;
-
-        CalcLightTrans(ref sParam);
-
         Vector4 lp = new Vector4(g_vLightPos.X, g_vLightPos.Y, g_vLightPos.Z, 1.0f);
         Matrix ls = g_matW * g_matLightTrans;
         effect.SetValue("matLS", ls);
         effect.SetValue("vLightPos", lp);
 
-        g_matW = Matrix.Scaling(5,5,5);
-        g_matL = Matrix.Translation(15,0,0);
+        effect.Technique = "Tec0_NormalDraw";
 
-        Vector3 vEye = new Vector3(0,0,-sParam.fCamLength);
-        Vector3 vCenter = new Vector3(0,0,0);
-        Vector3 vUp = new Vector3(0,1,0);
-        g_matV = Matrix.RotationYawPitchRoll(sParam.vCamAngle.Y, sParam.vCamAngle.X, 0.0f);
-        vEye = Vector3.TransformCoordinate(vEye, g_matV);
-        g_matV = Matrix.LookAtLH(vEye, vCenter, vUp);
-
-        g_matP = Matrix.PerspectiveFovLH(Geometry.DegreeToRadian(45.0f), 4.0f/3.0f, 1.0f, 200.0f);
-
+        //draw teapot
         Matrix wvp = g_matW * g_matV * g_matP;
         effect.SetValue("matWVP", wvp);
 
-        device.BeginScene();
-
-        device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.CornflowerBlue, 1.0f, 0);
-
-        int npass = effect.Begin(0);
-        for (int ipass = 0; ipass < npass; ipass++)
         {
-            effect.BeginPass(ipass);
-            teapot.DrawSubset(0);
-            effect.EndPass();
+            int npass = effect.Begin(0);
+            for (int ipass = 0; ipass < npass; ipass++)
+            {
+                effect.BeginPass(ipass);
+                teapot.DrawSubset(0);
+                effect.EndPass();
+            }
+            effect.End();
         }
-        effect.End();
+    }
 
-        device.EndScene();
+    void DrawReceiver()
+    {
+        effect.Technique = "Tec0_NormalDraw";
 
-        device.Present();
-        Thread.Sleep(30);
+        Matrix wvp;
+        Matrix ls;
+
+        //draw ground
+        wvp = g_matV * g_matP;
+        effect.SetValue("matWVP", wvp);
+        ls = g_matLightTrans;
+        effect.SetValue("matLS", ls);
+
+        device.VertexFormat = CustomVertex.PositionNormal.Format;
+
+        {
+            int npass = effect.Begin(0);
+            for (int ipass = 0; ipass < npass; ipass++)
+            {
+                effect.BeginPass(ipass);
+                device.SetStreamSource(0, g_pVBuffer, 0);
+                device.DrawPrimitives(PrimitiveType.TriangleStrip, 0, 2);
+                effect.EndPass();
+            }
+            effect.End();
+        }
+
+        //draw teapot
+        wvp = g_matW * g_matV * g_matP;
+        effect.SetValue("matWVP", wvp);
+        ls = g_matW * g_matLightTrans;
+        effect.SetValue("matLS", ls);
+
+        {
+            int npass = effect.Begin(0);
+            for (int ipass = 0; ipass < npass; ipass++)
+            {
+                effect.BeginPass(ipass);
+                teapot.DrawSubset(0);
+                effect.EndPass();
+            }
+            effect.End();
+        }
     }
 
     public void Dispose()
