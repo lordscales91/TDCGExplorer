11fbbc3d4d23a2339cc3aacd35ffa014cbbd9212
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-09-07 21:53:12 +0000

    added bmps controller

---
+++ trunk/batora/app/helpers/bmps_helper.rb
@@ -0,0 +1,2 @@
+module BmpsHelper
+end
--- trunk/batora/config/routes.rb
+++ trunk/batora/config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :bmps
   map.resources :characters
 
   # The priority is based upon order of creation: first created -> highest priority.
