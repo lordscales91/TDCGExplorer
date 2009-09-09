feedd7bf7f44d4bec83339f512f2172a8516c749
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-09-09 05:31:53 +0000

    fixed spec

--- trunk/batora/spec/routing/cards_routing_spec.rb
+++ trunk/batora/spec/routing/cards_routing_spec.rb
@@ -3,61 +3,61 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe CardsController do
   describe "route generation" do
     it "maps #index" do
-      route_for(:controller => "cards", :action => "index").should == "/cards"
+      route_for(:controller => "cards", :action => "index", :player_id => "1").should == "/players/1/cards"
     end
 
     it "maps #new" do
-      route_for(:controller => "cards", :action => "new").should == "/cards/new"
+      route_for(:controller => "cards", :action => "new", :player_id => "1").should == "/players/1/cards/new"
     end
 
     it "maps #show" do
-      route_for(:controller => "cards", :action => "show", :id => "1").should == "/cards/1"
+      route_for(:controller => "cards", :action => "show", :player_id => "1", :id => "1").should == "/players/1/cards/1"
     end
 
     it "maps #edit" do
-      route_for(:controller => "cards", :action => "edit", :id => "1").should == "/cards/1/edit"
+      route_for(:controller => "cards", :action => "edit", :player_id => "1", :id => "1").should == "/players/1/cards/1/edit"
     end
 
     it "maps #create" do
-      route_for(:controller => "cards", :action => "create").should == {:path => "/cards", :method => :post}
+      route_for(:controller => "cards", :action => "create", :player_id => "1").should == {:path => "/players/1/cards", :method => :post}
     end
 
     it "maps #update" do
-      route_for(:controller => "cards", :action => "update", :id => "1").should == {:path =>"/cards/1", :method => :put}
+      route_for(:controller => "cards", :action => "update", :player_id => "1", :id => "1").should == {:path =>"/players/1/cards/1", :method => :put}
     end
 
     it "maps #destroy" do
-      route_for(:controller => "cards", :action => "destroy", :id => "1").should == {:path =>"/cards/1", :method => :delete}
+      route_for(:controller => "cards", :action => "destroy", :player_id => "1", :id => "1").should == {:path =>"/players/1/cards/1", :method => :delete}
     end
   end
 
   describe "route recognition" do
     it "generates params for #index" do
-      params_from(:get, "/cards").should == {:controller => "cards", :action => "index"}
+      params_from(:get, "/players/1/cards").should == {:controller => "cards", :action => "index", :player_id => "1"}
     end
 
     it "generates params for #new" do
-      params_from(:get, "/cards/new").should == {:controller => "cards", :action => "new"}
+      params_from(:get, "/players/1/cards/new").should == {:controller => "cards", :action => "new", :player_id => "1"}
     end
 
     it "generates params for #create" do
-      params_from(:post, "/cards").should == {:controller => "cards", :action => "create"}
+      params_from(:post, "/players/1/cards").should == {:controller => "cards", :action => "create", :player_id => "1"}
     end
 
     it "generates params for #show" do
-      params_from(:get, "/cards/1").should == {:controller => "cards", :action => "show", :id => "1"}
+      params_from(:get, "/players/1/cards/1").should == {:controller => "cards", :action => "show", :player_id => "1", :id => "1"}
     end
 
     it "generates params for #edit" do
-      params_from(:get, "/cards/1/edit").should == {:controller => "cards", :action => "edit", :id => "1"}
+      params_from(:get, "/players/1/cards/1/edit").should == {:controller => "cards", :action => "edit", :player_id => "1", :id => "1"}
     end
 
     it "generates params for #update" do
-      params_from(:put, "/cards/1").should == {:controller => "cards", :action => "update", :id => "1"}
+      params_from(:put, "/players/1/cards/1").should == {:controller => "cards", :action => "update", :player_id => "1", :id => "1"}
     end
 
     it "generates params for #destroy" do
-      params_from(:delete, "/cards/1").should == {:controller => "cards", :action => "destroy", :id => "1"}
+      params_from(:delete, "/players/1/cards/1").should == {:controller => "cards", :action => "destroy", :player_id => "1", :id => "1"}
     end
   end
 end
