c665b733a73c8b6da83dfceec32e665eaf407e18
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-09-09 16:01:48 +0000

    authenticated system

--- batora/app/controllers/application.rb
+++ batora/app/controllers/application.rb
@@ -17,4 +17,6 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+
+  include AuthenticatedSystem
 end
