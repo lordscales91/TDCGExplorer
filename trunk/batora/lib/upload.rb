e7e665839ed4d34eb9bf2f1f7db412564a16b1f7
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-09-09 05:31:39 +0000

    upload: image crop and resize

--- trunk/batora/lib/upload.rb
+++ trunk/batora/lib/upload.rb
@@ -28,6 +28,14 @@ module_function
     png_path = RAILS_ROOT + "/public/images/bmps/#{bmp.id}.png"
     ilist.write(png_path)
 
+    image = ilist.first
+    image_1 = image.crop(0, 0, 128, 128)
+    image_1_path = RAILS_ROOT + "/public/images/bmps/128x128/#{bmp.id}.png"
+    image_1.write(image_1_path)
+    image_2 = image_1.resize(64, 64)
+    image_2_path = RAILS_ROOT + "/public/images/bmps/64x64/#{bmp.id}.png"
+    image_2.write(image_2_path)
+
     bmp
   end
 
