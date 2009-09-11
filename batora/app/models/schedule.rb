ed3dd0d268bb8e821cae2152c0dfb8aa3d83aa72
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-09-11 10:01:14 +0000

    moved fixtures

--- batora/app/models/schedule.rb
+++ batora/app/models/schedule.rb
@@ -1,4 +1,12 @@
 class Schedule < ActiveRecord::Base
   belongs_to :home_player, :class_name => "Player"
   belongs_to :away_player, :class_name => "Player"
+
+  def home_player_nick
+    home_player ? home_player.nick : nil
+  end
+
+  def away_player_nick
+    away_player ? away_player.nick : nil
+  end
 end
