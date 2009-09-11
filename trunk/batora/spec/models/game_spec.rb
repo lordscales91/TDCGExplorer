831db9d432565e88cec66337dd3368d07d4b8550
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-09-11 10:29:00 +0000

    moved fixtures

--- trunk/batora/spec/models/game_spec.rb
+++ trunk/batora/spec/models/game_spec.rb
@@ -1,13 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Game do
-  before(:each) do
-    @valid_attributes = {
-      :schedule => 
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    Game.create!(@valid_attributes)
-  end
 end
