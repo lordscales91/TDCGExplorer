234af518b295d173fabf2689495d40dc54f27bb5
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-09-23 00:34:12 +0000

    added TMOComposerConfig

---
+++ trunk/tsoview/TMOComposerConfig/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TMOComposerConfig
+{
+    public partial class Form1 : Form
+    {
+        public TSOConfig config;
+        string config_path = @"config.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+            config = TSOConfig.Load(config_path);
+            propertyGrid1.SelectedObject = config;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            config.Save(config_path);
+            Close();
+        }
+    }
+}
