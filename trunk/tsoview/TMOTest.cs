afe9bd8ef525feb43cb7656652368ff5d4bc2369
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-09-28 08:41:08 +0000

    YawPitchRoll each node

--- trunk/tsoview/TMOTest.cs
+++ trunk/tsoview/TMOTest.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using Direct3D=Microsoft.DirectX.Direct3D;
 using TDCG;
 
 class TMOTest
@@ -17,12 +20,13 @@ class TMOTest
 
         TMOFile tmo = new TMOFile();
         tmo.Load(source_file);
-        TMOFrame frame = tmo.frames[0];
-        foreach (TMOMat mat in frame.matrices)
+        foreach (TMONode node in tmo.nodes)
         {
+            TMOMat mat = node.frame_matrices[0];
+
             float yaw, pitch, roll;
             TMOMat.RotationToYawPitchRoll(ref mat.m, out yaw, out pitch, out roll);
-            Console.WriteLine("yaw {0:F2} pitch {1:F2} roll {2:F2}", yaw, pitch, roll);
+            Console.WriteLine("node {0} yaw {1:F2} pitch {2:F2} roll {3:F2}", node.ShortName, Geometry.RadianToDegree(yaw), Geometry.RadianToDegree(pitch), Geometry.RadianToDegree(roll));
         }
     }
 }
