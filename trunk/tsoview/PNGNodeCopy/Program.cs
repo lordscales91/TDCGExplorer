0ea9da3edfb3471ec1b852dc281f2f32811db130
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-10-19 15:41:16 +0000

    moved WriteTaOb to PNGWriter

--- trunk/tsoview/PNGNodeCopy/Program.cs
+++ trunk/tsoview/PNGNodeCopy/Program.cs
@@ -7,11 +7,15 @@ using TDCG;
 
 namespace PNGNodeCopy
 {
+    public class TSOData
+    {
+        internal uint opt1;
+        internal byte[] ftso;
+    }
+
     public class TSOFigure
     {
-        internal List<byte[]> TSODataList = new List<byte[]>();
-        internal List<byte[]> TSOOpt1List = new List<byte[]>();
-
+        internal List<TSOData> TSOList = new List<TSOData>();
         internal TMOFile tmo = null;
 
         internal byte[] lgta;
@@ -42,8 +46,6 @@ namespace PNGNodeCopy
         internal List<TSOFigure> TSOFigureList = new List<TSOFigure>();
 
         byte[] cami;
-
-        protected BinaryWriter writer;
 
         public bool CopyNode(string source_file, string motion_file, string node_name)
         {
@@ -105,8 +107,10 @@ namespace PNGNodeCopy
                     byte[] ftso = new byte[extract_length];
                     dest.Read(ftso, 0, extract_length);
 
-                    fig.TSODataList.Add(ftso);
-                    fig.TSOOpt1List.Add(opt1);
+                    TSOData tso = new TSOData();
+                    tso.opt1 = BitConverter.ToUInt32(opt1, 0);
+                    tso.ftso = ftso;
+                    fig.TSOList.Add(tso);
                 };
 
                 source.Load(source_file);
@@ -150,17 +154,17 @@ namespace PNGNodeCopy
             Console.WriteLine("Save File: " + dest_file);
             source.WriteTaOb += delegate(BinaryWriter bw)
             {
-                this.writer = bw;
+                PNGWriter pw = new PNGWriter(bw);
                 switch (source_type)
                 {
                 case "HSAV":
-                    WriteHsav();
+                    WriteHsav(pw);
                     break;
                 case "POSE":
-                    WritePose();
+                    WritePose(pw);
                     break;
                 case "SCNE":
-                    WriteScne();
+                    WriteScne(pw);
                     break;
                 }
             };
@@ -173,174 +177,45 @@ namespace PNGNodeCopy
             return true;
         }
 
-        protected void WriteHsav()
+        protected void WriteHsav(PNGWriter pw)
         {
-            WriteTDCG();
-            WriteHSAV();
+            pw.WriteTDCG();
+            pw.WriteHSAV();
             foreach (TSOFigure fig in TSOFigureList)
-                for (int i = 0; i < fig.TSODataList.Count; i++)
-                    WriteFTSO(fig.TSODataList[i], fig.TSOOpt1List[i]);
+                foreach (TSOData tso in fig.TSOList)
+                    pw.WriteFTSO(tso.opt1, tso.ftso);
         }
 
-        protected void WritePose()
+        protected void WritePose(PNGWriter pw)
         {
-            WriteTDCG();
-            WritePOSE();
-            WriteCAMI(cami);
+            pw.WriteTDCG();
+            pw.WritePOSE();
+            pw.WriteCAMI(cami);
             foreach (TSOFigure fig in TSOFigureList)
             {
-                WriteLGTA(fig.lgta);
-                WriteFTMO(fig.tmo);
+                pw.WriteLGTA(fig.lgta);
+                pw.WriteFTMO(fig.tmo);
             }
         }
 
-        protected void WriteScne()
+        protected void WriteScne(PNGWriter pw)
         {
-            WriteTDCG();
-            WriteSCNE();
-            WriteCAMI(cami);
+            pw.WriteTDCG();
+            pw.WriteSCNE(FigureCount());
+            pw.WriteCAMI(cami);
             foreach (TSOFigure fig in TSOFigureList)
             {
-                WriteLGTA(fig.lgta);
-                WriteFTMO(fig.tmo);
-                WriteFIGU(fig.figu);
-                for (int i = 0; i < fig.TSODataList.Count; i++)
-                    WriteFTSO(fig.TSODataList[i], fig.TSOOpt1List[i]);
+                pw.WriteLGTA(fig.lgta);
+                pw.WriteFTMO(fig.tmo);
+                pw.WriteFIGU(fig.figu);
+                foreach (TSOData tso in fig.TSOList)
+                    pw.WriteFTSO(tso.opt1, tso.ftso);
             }
         }
 
         protected int FigureCount()
         {
             return TSOFigureList.Count;
-        }
-
-        protected void WriteTaOb(string type, uint opt0, uint opt1, byte[] data)
-        {
-            //Console.WriteLine("WriteTaOb {0}", type);
-            //Console.WriteLine("taOb extract length {0}", data.Length);
-            byte[] chunk_type = System.Text.Encoding.ASCII.GetBytes(type);
-
-            MemoryStream dest = new MemoryStream();
-            using (DeflaterOutputStream gzip = new DeflaterOutputStream(dest))
-            {
-                gzip.IsStreamOwner = false;
-                gzip.Write(data, 0, data.Length);
-            }
-            dest.Seek(0, SeekOrigin.Begin);
-            //Console.WriteLine("taOb length {0}", dest.Length);
-            byte[] chunk_data = new byte[dest.Length + 20];
-
-            Array.Copy(chunk_type, 0, chunk_data, 0, 4);
-            byte[] buf;
-            buf = BitConverter.GetBytes((UInt32)opt0);
-            Array.Copy(buf, 0, chunk_data, 4, 4);
-            buf = BitConverter.GetBytes((UInt32)opt1);
-            Array.Copy(buf, 0, chunk_data, 8, 4);
-            buf = BitConverter.GetBytes((UInt32)data.Length);
-            Array.Copy(buf, 0, chunk_data, 12, 4);
-            buf = BitConverter.GetBytes((UInt32)dest.Length);
-            Array.Copy(buf, 0, chunk_data, 16, 4);
-
-            dest.Read(chunk_data, 20, (int)dest.Length);
-            PNGWriter.WriteChunk(writer, "taOb", chunk_data);
-        }
-
-        protected void WriteTaOb(string type, byte[] data)
-        {
-            WriteTaOb(type, 0, 0, data);
-        }
-
-        protected void WriteTDCG()
-        {
-            byte[] data = System.Text.Encoding.ASCII.GetBytes("$XP$");
-            WriteTaOb("TDCG", data);
-        }
-
-        protected void WriteHSAV()
-        {
-            byte[] data = System.Text.Encoding.ASCII.GetBytes("$XP$");
-            WriteTaOb("HSAV", data);
-        }
-
-        protected void WritePOSE()
-        {
-            byte[] data = System.Text.Encoding.ASCII.GetBytes("$XP$");
-            WriteTaOb("POSE", data);
-        }
-
-        protected void WriteSCNE()
-        {
-            byte[] data = System.Text.Encoding.ASCII.GetBytes("$XP$");
-            WriteTaOb("SCNE", 0, (uint)FigureCount(), data);
-        }
-
-        protected void WriteCAMI(byte[] data)
-        {
-            WriteTaOb("CAMI", data);
-        }
-
-        protected void WriteLGTA(byte[] data)
-        {
-            WriteTaOb("LGTA", data);
-        }
-
-        protected void WriteFIGU(byte[] data)
-        {
-            WriteTaOb("FIGU", data);
-        }
-
-        protected void WriteFile(string type, uint opt0, uint opt1, Stream source)
-        {
-            //Console.WriteLine("taOb extract length {0}", source.Length);
-
-            MemoryStream dest = new MemoryStream();
-            using (DeflaterOutputStream gzip = new DeflaterOutputStream(dest))
-            {
-                gzip.IsStreamOwner = false;
-
-                byte[] b = new byte[4096];
-                StreamUtils.Copy(source, gzip, b);
-            }
-            dest.Seek(0, SeekOrigin.Begin);
-            //Console.WriteLine("taOb length {0}", dest.Length);
-
-            byte[] chunk_type = System.Text.Encoding.ASCII.GetBytes(type);
-            byte[] chunk_data = new byte[dest.Length + 20];
-
-            Array.Copy(chunk_type, 0, chunk_data, 0, 4);
-
-            byte[] buf;
-            buf = BitConverter.GetBytes((UInt32)opt0);
-            Array.Copy(buf, 0, chunk_data, 4, 4);
-            buf = BitConverter.GetBytes((UInt32)opt1);
-            Array.Copy(buf, 0, chunk_data, 8, 4);
-
-            buf = BitConverter.GetBytes((UInt32)source.Length);
-            Array.Copy(buf, 0, chunk_data, 12, 4);
-            buf = BitConverter.GetBytes((UInt32)dest.Length);
-            Array.Copy(buf, 0, chunk_data, 16, 4);
-
-            dest.Read(chunk_data, 20, (int)dest.Length);
-            PNGWriter.WriteChunk(writer, "taOb", chunk_data);
-        }
-
-        protected void WriteFTMO(TMOFile tmo)
-        {
-            MemoryStream dest = new MemoryStream();
-            tmo.Save(dest);
-            dest.Seek(0, SeekOrigin.Begin);
-            WriteFile("FTMO", 0xADCFB72F, 0, dest);
-        }
-
-        protected uint opt_value(byte[] bytes)
-        {
-            return BitConverter.ToUInt32(bytes, 0);
-        }
-
-        protected void WriteFTSO(byte[] data, byte[] opt1)
-        {
-            MemoryStream dest = new MemoryStream(data);
-            WriteFile("FTSO", 0x26F5B8FE, opt_value(opt1), dest);
         }
     }
 }
