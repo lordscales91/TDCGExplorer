711d57341cc4565363d769c73f5aa762057adb88
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-11-13 02:32:46 +0000

    removed

--- branches/tsoview-019/build-TSOView.bat
+++
@@ -1,1 +0,0 @@
-csc /debug /d:DEBUG /out:build\TSOView.exe /m:TSOView.Program @tsoview.rsp @pngfile.rsp /r:CSScriptLibrary.dll TDCG\Camera.cs TDCG\Figure.cs TDCG\FigureForm.cs TDCG\CameraMotion.cs TDCG\FigureMotion.cs TDCG\Viewer.cs TSOView\TSOConfig.cs TSOView\TSOForm.cs TSOView\Program.cs
