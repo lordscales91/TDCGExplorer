0efe7b627377cfa46d103cecc813d243ba27d49c
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-11-13 02:32:19 +0000

    removed

--- branches/tsoview-TMOProportion/build-CameraLoad.bat
+++
@@ -1,1 +0,0 @@
-csc /out:build\CameraLoad.exe @direct3d.rsp TDCG\Camera.cs TDCG\CameraMotion.cs CameraLoad\Program.cs
--- branches/tsoview-TMOProportion/build-PNGNodeCopy.bat
+++
@@ -1,1 +0,0 @@
-csc /out:build\PNGNodeCopy.exe @tsoview.rsp @pngfile.rsp PNGNodeCopy\Program.cs
