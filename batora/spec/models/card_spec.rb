89f83003c3c90e66dc3b44c94f5d9e07b7196ec1
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2009-12-07 18:54:45 +0000

    tagging tsoview-026

--- batora/spec/models/card_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
-describe Card, "player" do
-  fixtures :cards, :players
-
-  it "cards(:one).player ‚Í players(:konoa) ‚Å‚ ‚é" do
-    cards(:one).player.should == players(:konoa)
-  end
-end
