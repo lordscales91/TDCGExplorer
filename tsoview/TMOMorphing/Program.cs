90de935aae161eb537c854ae35781c50f7b71392
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2010-02-05 12:26:11 +0000

    added TMOMorphing

---
+++ tsoview/TMOMorphing/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TMOMorphing
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            TSOConfig tso_config;
+
+            string tso_config_file = Path.Combine(Application.StartupPath, @"config.xml");
+            if (File.Exists(tso_config_file))
+                tso_config = TSOConfig.Load(tso_config_file);
+            else
+                tso_config = new TSOConfig();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(tso_config, args));
+        }
+    }
+}
