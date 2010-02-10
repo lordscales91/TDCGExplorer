7c457f23e45e787ca6c74e92779d39e7111aee6d
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2010-02-10 01:34:30 +0000

    FindVertexOnScreenPoint

--- TDCG/WeightViewer.cs
+++ TDCG/WeightViewer.cs
@@ -16,5 +16,119 @@ namespace TDCG
     /// </summary>
 public class WeightViewer : Viewer
 {
+    /// 球とレイの衝突を見つけます。
+    public bool DetectSphereRayCollision(float sphereRadius, ref Vector3 sphereCenter, ref Vector3 rayStart, ref Vector3 rayOrientation, out Vector3 collisionPoint, out float collisionTime)
+    {
+        collisionTime = 0.0f;
+        collisionPoint = Vector3.Empty;
+
+        Vector3 u = rayStart - sphereCenter;
+        float a = Vector3.Dot(rayOrientation, rayOrientation);
+        float b = Vector3.Dot(rayOrientation, u);
+        float c = Vector3.Dot(u, u) - sphereRadius*sphereRadius;
+        if (a <= float.Epsilon)
+            //誤差
+            return false;
+        float d = b*b - a*c;
+        if (d < 0.0f)
+            //衝突しない
+            return false;
+        collisionTime = (-b - (float)Math.Sqrt(d))/a;
+        collisionPoint = rayStart + rayOrientation*collisionTime;
+        return true;
+    }
+
+    /// <summary>
+    /// viewport行列を作成します。
+    /// </summary>
+    /// <param name="viewport">viewport</param>
+    /// <returns>viewport行列</returns>
+    public Matrix CreateViewportMatrix(Viewport viewport)
+    {
+        Matrix m = Matrix.Identity;
+        m.M11 = (float)viewport.Width / 2;
+        m.M22 = -1.0f * (float)viewport.Height / 2;
+        m.M33 = (float)viewport.MaxZ - (float)viewport.MinZ;
+        m.M41 = (float)(viewport.X + viewport.Width / 2);
+        m.M42 = (float)(viewport.Y + viewport.Height / 2);
+        m.M43 = viewport.MinZ;
+        return m;
+    }
+
+    /// スクリーン位置をワールド座標へ変換します。
+    public Vector3 ScreenToWorld(float screenX, float screenY, float z, Viewport viewport, Matrix view, Matrix proj)
+    {
+        //スクリーン位置
+        Vector3 v = new Vector3(screenX, screenY,  z);
+
+        Matrix inv_m = Matrix.Invert(CreateViewportMatrix(viewport));
+        Matrix inv_proj = Matrix.Invert(proj);
+        Matrix inv_view = Matrix.Invert(view);
+
+        //スクリーン位置をワールド座標へ変換
+        return Vector3.TransformCoordinate(v, inv_m * inv_proj * inv_view);
+    }
+
+    /// スクリーン位置をワールド座標へ変換します。
+    public Vector3 ScreenToWorld(float screenX, float screenY, float z)
+    {
+        return ScreenToWorld(screenX, screenY, z, device.Viewport, Transform_View, Transform_Projection);
+    }
+
+    public static Vector3 CalcSkindeformPosition(ref Vertex v, Matrix[] boneMatrices)
+    {
+        Vector3 pos = Vector3.Empty;
+        for (int i = 0; i < 4; i++)
+        {
+            Matrix m = boneMatrices[v.skin_weights[i].bone_index];
+            float w = v.skin_weights[i].weight;
+            pos += Vector3.TransformCoordinate(v.position, m) * w;
+        }
+        return pos;
+    }
+
+    /// スクリーン座標から頂点を見つけます。
+    /// 衝突する頂点の中で最も近い位置にある頂点を返します。
+    private int FindVertexOnScreenPoint(float x, float y, Figure fig, TSOMesh mesh)
+    {
+        int vertex_id = -1;
+
+        Matrix[] clipped_boneMatrices = new Matrix[mesh.maxPalettes];
+
+        for (int numPalettes = 0; numPalettes < mesh.maxPalettes; numPalettes++)
+        {
+            TSONode tso_node = mesh.GetBone(numPalettes);
+            TMONode tmo_node;
+            if (fig.nodemap.TryGetValue(tso_node, out tmo_node))
+                clipped_boneMatrices[numPalettes] = tso_node.OffsetMatrix * tmo_node.combined_matrix;
+        }
+
+        {
+            Vector3 collisionPoint;
+            float collisionTime;
+            float min_time = 1e12f;
+
+            float sphereRadius = 0.25f * 0.1f;
+            Vector3 rayStart = ScreenToWorld(x, y, 0.0f);
+            Vector3 rayEnd = ScreenToWorld(x, y, 1.0f);
+            Vector3 rayOrientation = rayEnd - rayStart;
+
+            for (int i = 0; i < mesh.vertices.Length; i++)
+            {
+                Vector3 sphereCenter = CalcSkindeformPosition(ref mesh.vertices[i], clipped_boneMatrices);
+                if (DetectSphereRayCollision(sphereRadius, ref sphereCenter, ref rayStart, ref rayOrientation, out collisionPoint, out collisionTime))
+                {
+                    if (collisionTime < min_time)
+                    {
+                        min_time = collisionTime;
+                        vertex_id = i;
+                    }
+                }
+            }
+        }
+
+        return vertex_id;
+    }
+
 }
 }
