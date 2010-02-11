217d6c4732fd8c3a2ed1864f42ea4a071d5e637d
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2010-02-11 18:04:05 +0000

    BoneHeatingViewSwitch

--- branches/tsoview-TSOWeight/TSOWeight/Form1.Designer.cs
+++ branches/tsoview-TSOWeight/TSOWeight/Form1.Designer.cs
@@ -43,6 +43,7 @@
             this.lbBoneIndices = new System.Windows.Forms.Label();
             this.lvBoneIndices = new System.Windows.Forms.ListView();
             this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.cbBoneHeatingView = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // lvFrames
@@ -133,12 +134,24 @@
             // 
             this.columnHeader3.Text = "Name";
             // 
+            // cbBoneHeatingView
+            // 
+            this.cbBoneHeatingView.AutoSize = true;
+            this.cbBoneHeatingView.Location = new System.Drawing.Point(138, 300);
+            this.cbBoneHeatingView.Name = "cbBoneHeatingView";
+            this.cbBoneHeatingView.Size = new System.Drawing.Size(48, 16);
+            this.cbBoneHeatingView.TabIndex = 7;
+            this.cbBoneHeatingView.Text = "Heat";
+            this.cbBoneHeatingView.UseVisualStyleBackColor = true;
+            this.cbBoneHeatingView.CheckedChanged += new System.EventHandler(this.cbBoneHeatingView_CheckedChanged);
+            // 
             // Form1
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 563);
+            this.Controls.Add(this.cbBoneHeatingView);
             this.Controls.Add(this.lbBoneIndices);
             this.Controls.Add(this.lvBoneIndices);
             this.Controls.Add(this.lbMeshes);
@@ -150,6 +163,7 @@
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
             this.DragOver += new System.Windows.Forms.DragEventHandler(this.Form1_DragOver);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -165,6 +179,7 @@
         private System.Windows.Forms.Label lbBoneIndices;
         private System.Windows.Forms.ListView lvBoneIndices;
         private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.CheckBox cbBoneHeatingView;
     }
 }
 
