0df6e1023b5c28774e310a89010525e4ff4493f6
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2010-02-11 17:13:27 +0000

    AssignFrames

--- TSOWeight/Form1.Designer.cs
+++ TSOWeight/Form1.Designer.cs
@@ -33,11 +33,68 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lvFrames = new System.Windows.Forms.ListView();
+            this.lbFrames = new System.Windows.Forms.Label();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lvFrames
+            // 
+            this.lvFrames.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lvFrames.FullRowSelect = true;
+            this.lvFrames.GridLines = true;
+            this.lvFrames.HideSelection = false;
+            this.lvFrames.Location = new System.Drawing.Point(12, 24);
+            this.lvFrames.MultiSelect = false;
+            this.lvFrames.Name = "lvFrames";
+            this.lvFrames.Size = new System.Drawing.Size(180, 120);
+            this.lvFrames.TabIndex = 0;
+            this.lvFrames.UseCompatibleStateImageBehavior = false;
+            this.lvFrames.View = System.Windows.Forms.View.Details;
+            // 
+            // lbFrames
+            // 
+            this.lbFrames.AutoSize = true;
+            this.lbFrames.Location = new System.Drawing.Point(10, 9);
+            this.lbFrames.Name = "lbFrames";
+            this.lbFrames.Size = new System.Drawing.Size(43, 12);
+            this.lbFrames.TabIndex = 1;
+            this.lbFrames.Text = "Frames";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 563);
+            this.Controls.Add(this.lbFrames);
+            this.Controls.Add(this.lvFrames);
+            this.Name = "Form1";
+            this.Text = "TSOWeight";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.Form1_DragOver);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lvFrames;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Label lbFrames;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
