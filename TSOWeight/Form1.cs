dd663c57425e9f0372f2c21f833426f32fecfb4e
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2010-02-11 16:49:06 +0000

    setup WeightViewer

--- TSOWeight/Form1.cs
+++ TSOWeight/Form1.cs
@@ -6,15 +6,24 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using TDCG;
 
 namespace TSOWeight
 {
     public partial class Form1 : Form
     {
+        WeightViewer viewer = null;
+
         public Form1(TSOConfig tso_config, string[] args)
         {
             InitializeComponent();
             this.ClientSize = tso_config.ClientSize;
+
+            this.viewer = new WeightViewer();
+
+            if (viewer.InitializeApplication(this))
+            {
+            }
         }
     }
 }
