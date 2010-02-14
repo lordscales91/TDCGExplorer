21987953b50ef94851e34f660db04b4b186b68b9
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2010-02-14 17:07:08 +0000

    renamed

--- trunk/tsoview/TSOWeight/Form1.Designer.cs
+++ trunk/tsoview/TSOWeight/Form1.Designer.cs
@@ -72,11 +72,11 @@
             this.lbBoneIndices = new System.Windows.Forms.Label();
             this.lvBoneIndices = new System.Windows.Forms.ListView();
             this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.lbSubMeshes = new System.Windows.Forms.Label();
             this.lbMeshes = new System.Windows.Forms.Label();
-            this.lbFrames = new System.Windows.Forms.Label();
-            this.lvMeshes = new System.Windows.Forms.ListView();
+            this.lvSubMeshes = new System.Windows.Forms.ListView();
             this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
-            this.lvFrames = new System.Windows.Forms.ListView();
+            this.lvMeshes = new System.Windows.Forms.ListView();
             this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btnWeight = new System.Windows.Forms.Button();
@@ -352,10 +352,10 @@
             this.panel1.Controls.Add(this.lvTSOFiles);
             this.panel1.Controls.Add(this.lbBoneIndices);
             this.panel1.Controls.Add(this.lvBoneIndices);
+            this.panel1.Controls.Add(this.lbSubMeshes);
             this.panel1.Controls.Add(this.lbMeshes);
-            this.panel1.Controls.Add(this.lbFrames);
+            this.panel1.Controls.Add(this.lvSubMeshes);
             this.panel1.Controls.Add(this.lvMeshes);
-            this.panel1.Controls.Add(this.lvFrames);
             this.panel1.Location = new System.Drawing.Point(0, 26);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(144, 681);
@@ -417,57 +417,57 @@
             // 
             this.columnHeader3.Text = "Name";
             // 
+            // lbSubMeshes
+            // 
+            this.lbSubMeshes.Location = new System.Drawing.Point(12, 277);
+            this.lbSubMeshes.Name = "lbSubMeshes";
+            this.lbSubMeshes.Size = new System.Drawing.Size(120, 12);
+            this.lbSubMeshes.TabIndex = 11;
+            this.lbSubMeshes.Text = "Sub meshes";
+            // 
             // lbMeshes
             // 
-            this.lbMeshes.Location = new System.Drawing.Point(12, 277);
+            this.lbMeshes.Location = new System.Drawing.Point(12, 139);
             this.lbMeshes.Name = "lbMeshes";
             this.lbMeshes.Size = new System.Drawing.Size(120, 12);
-            this.lbMeshes.TabIndex = 11;
+            this.lbMeshes.TabIndex = 10;
             this.lbMeshes.Text = "Meshes";
             // 
-            // lbFrames
+            // lvSubMeshes
             // 
-            this.lbFrames.Location = new System.Drawing.Point(12, 139);
-            this.lbFrames.Name = "lbFrames";
-            this.lbFrames.Size = new System.Drawing.Size(120, 12);
-            this.lbFrames.TabIndex = 10;
-            this.lbFrames.Text = "Frames";
-            // 
-            // lvMeshes
-            // 
-            this.lvMeshes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvSubMeshes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader2});
-            this.lvMeshes.FullRowSelect = true;
-            this.lvMeshes.GridLines = true;
-            this.lvMeshes.HideSelection = false;
-            this.lvMeshes.Location = new System.Drawing.Point(12, 292);
-            this.lvMeshes.MultiSelect = false;
-            this.lvMeshes.Name = "lvMeshes";
-            this.lvMeshes.Size = new System.Drawing.Size(120, 120);
-            this.lvMeshes.TabIndex = 9;
-            this.lvMeshes.UseCompatibleStateImageBehavior = false;
-            this.lvMeshes.View = System.Windows.Forms.View.Details;
-            this.lvMeshes.SelectedIndexChanged += new System.EventHandler(this.lvMeshes_SelectedIndexChanged);
+            this.lvSubMeshes.FullRowSelect = true;
+            this.lvSubMeshes.GridLines = true;
+            this.lvSubMeshes.HideSelection = false;
+            this.lvSubMeshes.Location = new System.Drawing.Point(12, 292);
+            this.lvSubMeshes.MultiSelect = false;
+            this.lvSubMeshes.Name = "lvSubMeshes";
+            this.lvSubMeshes.Size = new System.Drawing.Size(120, 120);
+            this.lvSubMeshes.TabIndex = 9;
+            this.lvSubMeshes.UseCompatibleStateImageBehavior = false;
+            this.lvSubMeshes.View = System.Windows.Forms.View.Details;
+            this.lvSubMeshes.SelectedIndexChanged += new System.EventHandler(this.lvMeshes_SelectedIndexChanged);
             // 
             // columnHeader2
             // 
             this.columnHeader2.Text = "Name";
             // 
-            // lvFrames
+            // lvMeshes
             // 
-            this.lvFrames.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvMeshes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
-            this.lvFrames.FullRowSelect = true;
-            this.lvFrames.GridLines = true;
-            this.lvFrames.HideSelection = false;
-            this.lvFrames.Location = new System.Drawing.Point(12, 154);
-            this.lvFrames.MultiSelect = false;
-            this.lvFrames.Name = "lvFrames";
-            this.lvFrames.Size = new System.Drawing.Size(120, 120);
-            this.lvFrames.TabIndex = 8;
-            this.lvFrames.UseCompatibleStateImageBehavior = false;
-            this.lvFrames.View = System.Windows.Forms.View.Details;
-            this.lvFrames.SelectedIndexChanged += new System.EventHandler(this.lvFrames_SelectedIndexChanged);
+            this.lvMeshes.FullRowSelect = true;
+            this.lvMeshes.GridLines = true;
+            this.lvMeshes.HideSelection = false;
+            this.lvMeshes.Location = new System.Drawing.Point(12, 154);
+            this.lvMeshes.MultiSelect = false;
+            this.lvMeshes.Name = "lvMeshes";
+            this.lvMeshes.Size = new System.Drawing.Size(120, 120);
+            this.lvMeshes.TabIndex = 8;
+            this.lvMeshes.UseCompatibleStateImageBehavior = false;
+            this.lvMeshes.View = System.Windows.Forms.View.Details;
+            this.lvMeshes.SelectedIndexChanged += new System.EventHandler(this.lvFrames_SelectedIndexChanged);
             // 
             // columnHeader1
             // 
@@ -700,11 +700,11 @@
         private System.Windows.Forms.Label lbBoneIndices;
         private System.Windows.Forms.ListView lvBoneIndices;
         private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label lbSubMeshes;
         private System.Windows.Forms.Label lbMeshes;
-        private System.Windows.Forms.Label lbFrames;
-        private System.Windows.Forms.ListView lvMeshes;
+        private System.Windows.Forms.ListView lvSubMeshes;
         private System.Windows.Forms.ColumnHeader columnHeader2;
-        private System.Windows.Forms.ListView lvFrames;
+        private System.Windows.Forms.ListView lvMeshes;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label lbTSOFiles;
