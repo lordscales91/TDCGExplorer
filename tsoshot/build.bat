b450bac85b1c0eed4e49c0f28f858a539fa9ef3a
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2010-02-16 14:20:31 +0000

    tagging tsoview TSOWeight-003

--- tsoshot/build.bat
+++
@@ -1,1 +0,0 @@
-csc /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:Microsoft.DirectX.Direct3DX.dll,Microsoft.DirectX.Direct3D.dll,Microsoft.DirectX.dll /r:ArchiveLib.dll /r:ICSharpCode.SharpZipLib.DLL MersenneTwister.cs LZSS.cs TAHCryptStream.cs TAHUtil.cs TAHFile.cs TSOFile.cs TMOFile.cs TMOWriter.cs PNGFile.cs PNGWriter.cs Shader.cs TSOCamera.cs TSOShot.cs
