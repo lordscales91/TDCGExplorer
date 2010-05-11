43567d03c6eb9a33c50fe1744848b6d5f9a524f6
3dcustom <3dcustom@72ae2088-5f33-de11-a17b-0000e250a282>
2010-05-11 10:09:24 +0000

    added TSOHairPart

--- tsoview/TDCG.TSOHair/TSOHairProcessor.cs
+++ tsoview/TDCG.TSOHair/TSOHairProcessor.cs
@@ -8,42 +8,68 @@ using TDCG;
 
 namespace TDCG.TSOHair
 {
+    public class TSOHairPart
+    {
+        Regex re = null;
+        string text_pattern = null;
+
+        public string Name { get; set; }
+        public string TextPattern
+        {
+            get
+            {
+                return text_pattern;
+            }
+            set
+            {
+                text_pattern = value;
+                re = new Regex(text_pattern, RegexOptions.IgnoreCase);
+            }
+        }
+        public string SubPath { get; set; }
+        public string TexPath { get; set; }
+
+        public Regex GetTextRegex()
+        {
+            return re;
+        }
+    }
+
     public class TSOHairProcessor
     {
-        public enum Type { Unknown, Kami, Housen, Ribbon };
+        List<TSOHairPart> parts = new List<TSOHairPart>();
 
-        static Regex re_kami = new Regex(@"kami", RegexOptions.IgnoreCase);
-        static Regex re_housen = new Regex(@"housen|w_faceparts", RegexOptions.IgnoreCase);
-        static Regex re_ribbon = new Regex(@"ribb?on", RegexOptions.IgnoreCase);
+        public TSOHairProcessor()
+        {
+            TSOHairPart part;
+
+            // Housen ÇÕ Kami ÇÊÇËóDêÊÇ∑ÇÈÅB
+            // ex. KamiHousen ÇÃèÍçáÇÕ Housen Ç∆ÇµÇƒèàóù
+            part = new TSOHairPart();
+            part.Name = "Housen";
+            part.TextPattern = @"housen|w_faceparts";
+            part.SubPath = @"Cgfx_kage\Cgfxkage_{0}";
+            part.TexPath = @"image_kage\KIT_KAGE_{0}.bmp";
+            parts.Add(part);
+
+            part = new TSOHairPart();
+            part.Name = "Ribbon";
+            part.TextPattern = @"ribb?on";
+            part.SubPath = null;
+            part.TexPath = @"image_ribon\KIT_RIBON_{0}.bmp";
+            parts.Add(part);
+
+            part = new TSOHairPart();
+            part.Name = "Kami";
+            part.TextPattern = @"kami";
+            part.SubPath = @"Cgfx_kami\Cgfxkami_{0}";
+            part.TexPath = @"image_kami\KIT_BASE_{0}.bmp";
+            parts.Add(part);
+        }
 
         public static string GetHairKitPath()
         {
             return Path.Combine(Application.StartupPath, @"HAIR_KIT");
-        }
-
-        public static string GetKamiSubPath(string col)
-        {
-            return Path.Combine(GetHairKitPath(), string.Format(@"Cgfx_kami\Cgfxkami_{0}", col));
-        }
-
-        public static string GetKageSubPath(string col)
-        {
-            return Path.Combine(GetHairKitPath(), string.Format(@"Cgfx_kage\Cgfxkage_{0}", col));
-        }
-
-        public static string GetKamiTexPath(string col)
-        {
-            return Path.Combine(GetHairKitPath(), string.Format(@"image_kami\KIT_BASE_{0}.bmp", col));
-        }
-
-        public static string GetKageTexPath(string col)
-        {
-            return Path.Combine(GetHairKitPath(), string.Format(@"image_kage\KIT_KAGE_{0}.bmp", col));
-        }
-
-        public static string GetRibonTexPath(string col)
-        {
-            return Path.Combine(GetHairKitPath(), string.Format(@"image_ribon\KIT_RIBON_{0}.bmp", col));
         }
 
         public void Process(TSOFile tso, string col)
@@ -58,86 +84,56 @@ namespace TDCG.TSOHair
             foreach (TSOSubScript sub in tso.sub_scripts)
             {
                 Console.WriteLine("  sub name {0} file {1}", sub.Name, sub.FileName);
-                Type type = Type.Unknown;
+                TSOHairPart detected_part = null;
 
                 Shader shader = sub.shader;
                 string color_tex_name = shader.ColorTexName;
                 string shade_tex_name = shader.ShadeTexName;
 
-                // Housen ÇÕ Kami ÇÊÇËóDêÊÇ∑ÇÈÅB
-                // ex. KamiHousen ÇÃèÍçáÇÕ Housen Ç∆ÇµÇƒèàóù
-                if (re_housen.IsMatch(sub.Name))
+                foreach (TSOHairPart part in parts)
                 {
-                    type = Type.Housen;
+                    if (part.GetTextRegex().IsMatch(sub.Name))
+                    {
+                        detected_part = part;
+                        break;
+                    }
                 }
-                else
-                if (re_ribbon.IsMatch(sub.Name))
-                {
-                    type = Type.Ribbon;
-                }
-                else
-                if (re_kami.IsMatch(sub.Name))
-                {
-                    type = Type.Kami;
-                }
-                else
+
+                if (detected_part == null)
                 {
                     TSOTex colorTex;
                     if (texmap.TryGetValue(color_tex_name, out colorTex))
                     {
                         string file = colorTex.FileName.Trim('"');
 
-                        if (re_housen.IsMatch(file))
+                        foreach (TSOHairPart part in parts)
                         {
-                            type = Type.Housen;
-                        }
-                        else
-                        if (re_ribbon.IsMatch(file))
-                        {
-                            type = Type.Ribbon;
-                        }
-                        else
-                        if (re_kami.IsMatch(file))
-                        {
-                            type = Type.Kami;
+                            if (part.GetTextRegex().IsMatch(file))
+                            {
+                                detected_part = part;
+                                break;
+                            }
                         }
                     }
                 }
-                Console.WriteLine("    : type {0}", type);
 
-                switch (type)
+                Console.WriteLine("    : type {0}", (detected_part != null) ? detected_part.Name : "Unknown");
+
+                if (detected_part != null)
                 {
-                    case Type.Kami:
-                        {
-                            sub.Load(GetKamiSubPath(col));
+                    if (detected_part.SubPath != null)
+                    {
+                        sub.Load(Path.Combine(GetHairKitPath(), string.Format(detected_part.SubPath, col)));
+                    }
 
-                            TSOTex colorTex;
-                            if (texmap.TryGetValue(color_tex_name, out colorTex))
-                            {
-                                colorTex.Load(GetKamiTexPath(col));
-                            }
-                        }
-                        break;
-                    case Type.Housen:
+                    if (detected_part.TexPath != null)
+                    {
+                        TSOTex colorTex;
+                        if (texmap.TryGetValue(color_tex_name, out colorTex))
                         {
-                            sub.Load(GetKageSubPath(col));
-
-                            TSOTex colorTex;
-                            if (texmap.TryGetValue(color_tex_name, out colorTex))
-                            {
-                                colorTex.Load(GetKageTexPath(col));
-                            }
+                            colorTex.Load(Path.Combine(GetHairKitPath(), string.Format(detected_part.TexPath, col)));
                         }
-                        break;
-                    case Type.Ribbon:
-                        {
-                            TSOTex colorTex;
-                            if (texmap.TryGetValue(color_tex_name, out colorTex))
-                            {
-                                colorTex.Load(GetRibonTexPath(col));
-                            }
-                        }
-                        break;
+                    }
                 }
 
                 sub.GenerateShader();
