4bee98c344970b131d380a800a9db4caa17de580
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2010-06-19 09:34:47 +0000

    added relationships/recent

--- trunk/marimo/app/controllers/relationships_controller.rb
+++ trunk/marimo/app/controllers/relationships_controller.rb
@@ -2,6 +2,11 @@ class RelationshipsController < ApplicationController
   def index
   end
 
+  def recent
+    @relationships = Relationship.find(:all, :conditions => ["updated_at > ?", 3.days.ago])
+    render :xml => @relationships
+  end
+
   def show
   end
 
