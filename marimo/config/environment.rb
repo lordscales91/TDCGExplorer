7f9e3dd49359b07f5c8c0ee1c79fc8b6bd3f901f
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2010-06-19 03:41:42 +0000

    update rails-2.3.5 gettext-2.1.0

--- marimo/config/environment.rb
+++ marimo/config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -22,9 +22,12 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
-  config.gem "gettext", :lib => "gettext/rails", :version => "= 1.93.0"
-  # config.gem "rspec-rails", :lib => "spec"
   config.gem "will_paginate", :lib => "will_paginate"
+  config.gem "locale"
+  config.gem "locale_rails", :version => "2.0.5"
+  config.gem "gettext"
+  config.gem "gettext_activerecord", :version => "2.1.0"
+  config.gem "gettext_rails", :version => "2.1.0"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
