dc8aaf0c2b5ae7a69b08ef7849ba148477dd4421
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2010-06-19 08:00:58 +0000

    mirror tsos

--- marimo-remoting/foo.rb
+++ marimo-remoting/foo.rb
@@ -10,11 +10,17 @@ module Remoting
     # self.site = 'http://localhost:3000'
     self.site = 'http://3dcustom.ath.cx/rails'
   end
+  class Tso < ActiveResource::Base
+    # self.site = 'http://localhost:3000'
+    self.site = 'http://3dcustom.ath.cx/rails'
+  end
 end
 rarcs = Remoting::Arc.find(:all, :from => :recent)
 p rarcs.size
 rtahs = Remoting::Tah.find(:all, :from => :recent)
 p rtahs.size
+rtsos = Remoting::Tso.find(:all, :from => :recent)
+p rtsos.size
 gem 'activerecord'
 require 'active_record'
 config = YAML.load(IO.read('config/database.yml'))
@@ -41,4 +47,13 @@ rtahs.each do |rtah|
   attributes.delete('id')
   p attributes
   p tah.update_attributes(attributes)
+end
+class Tso < ActiveRecord::Base
+end
+rtsos.each do |rtso|
+  tso = Tso.find_or_initialize_by_id(rtso.id)
+  attributes = rtso.attributes
+  attributes.delete('id')
+  p attributes
+  p tso.update_attributes(attributes)
 end
