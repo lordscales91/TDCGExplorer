644c810b86a47a0273c183133b95c7f50b8598a4
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2010-07-01 22:43:48 +0000

    Form1.LoadMqoFile

--- mqoview/Form1.cs
+++ mqoview/Form1.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
+using System.IO;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Microsoft.DirectX;
@@ -35,6 +34,60 @@ namespace mqoview
             base.OnPaint(e);
             device.Clear(ClearFlags.Target, Color.CornflowerBlue, 1.0f, 0);
             device.Present();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                foreach (string src in (string[])e.Data.GetData(DataFormats.FileDrop))
+                    LoadAnyFile(src, (e.KeyState & 8) == 8);
+            }
+        }
+
+        private void Form1_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                if ((e.KeyState & 8) == 8)
+                    e.Effect = DragDropEffects.Copy;
+                else
+                    e.Effect = DragDropEffects.Move;
+            }
+        }
+
+        /// <summary>
+        /// 任意のファイルを読み込みます。
+        /// </summary>
+        /// <param name="source_file">任意のパス</param>
+        public void LoadAnyFile(string source_file)
+        {
+            LoadAnyFile(source_file, false);
+        }
+
+        /// <summary>
+        /// 任意のファイルを読み込みます。
+        /// </summary>
+        /// <param name="source_file">任意のパス</param>
+        /// <param name="append">FigureListを消去せずに追加するか</param>
+        public void LoadAnyFile(string source_file, bool append)
+        {
+            switch (Path.GetExtension(source_file).ToLower())
+            {
+                case ".mqo":
+                    LoadMqoFile(source_file);
+                    break;
+            }
+        }
+
+        public void LoadMqoFile(string source_file)
+        {
+            MqoFile mqo = new MqoFile();
+            mqo.Load(source_file);
+            foreach (MqoObject obj in mqo.Objects)
+            {
+                obj.WriteBuffer(device);
+            }
         }
     }
 }
