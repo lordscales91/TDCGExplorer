677efda0cf73cd2edd3d1cec1afeb2a32e67cf49
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2010-07-01 21:05:37 +0000

    imported

---
+++ mqoview/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace mqoview
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            using (Form1 form1 = new Form1())
+            {
+                // Show our form and initialize our graphics engine
+                form1.Show();
+                form1.InitializeGraphics();
+                Application.Run(form1);
+            }
+        }
+    }
+}
