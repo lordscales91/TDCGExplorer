ce75efad20be64932a8a4073a962527a2dbbd9fe
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2010-07-30 13:57:56 +0000

    tagging tsoview-031

--- tahtool/build-TAHTool.bat
+++
@@ -1,3 +0,0 @@
-csc /platform:x86 /unsafe /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:Microsoft.DirectX.Direct3D.dll,Microsoft.DirectX.dll TMOFile.cs TMOWriter.cs Boin.cs TAHBoin.cs
-csc /platform:x86 /unsafe /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:Microsoft.DirectX.Direct3D.dll,Microsoft.DirectX.dll TMOFile.cs TMOWriter.cs Dom.cs TAHDom.cs
-csc /platform:x86 /unsafe /lib:"C:\WINDOWS\Microsoft.NET\DirectX for Managed Code\1.0.2902.0" /r:Microsoft.DirectX.Direct3D.dll,Microsoft.DirectX.dll TMOFile.cs TMOWriter.cs Pedo.cs TAHPedo.cs
