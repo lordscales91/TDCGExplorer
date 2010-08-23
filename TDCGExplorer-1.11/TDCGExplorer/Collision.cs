ca1b227b1b9f3bbb83e025d34b1f1231b31aef17
konoa <konoa@72ae2088-5f33-de11-a17b-0000e250a282>
2010-08-23 01:06:13 +0000

    Clean up source repositry, and for next generation.

---
+++ TDCGExplorer-1.11/TDCGExplorer/Collision.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDCGExplorer
+{
+    public class CollisionItem
+    {
+        public ArcsTahEntry tah;
+        public List<ArcsCollisionRecord> entries;
+    }
+}
---
+++ TDCGExplorer-1.11/TDCGExplorer/GenTahInfo.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDCGExplorer
+{
+    // TAH情報を抽象化する.
+    public class GenericTahInfo
+    {
+        public virtual int id
+        {
+            get { return -1; }
+            set { }
+        }
+        public virtual int zipid
+        {
+            get { return -1; }
+            set { }
+        }
+        public virtual string path
+        {
+            get { return null; }
+            set { }
+        }
+        public virtual string shortname
+        {
+            get { return null; }
+            set { }
+        }
+        public virtual int version
+        {
+            get { return -1; }
+            set { }
+        }
+    }
+
+    public class GenericArcsTahInfo : GenericTahInfo
+    {
+        private ArcsTahEntry tahEntry;
+        public GenericArcsTahInfo(ArcsTahEntry entry)
+        {
+            tahEntry = entry;
+        }
+        public override int id
+        {
+            get { return tahEntry.id; }
+            set { }
+        }
+        public override string path
+        {
+            get { return tahEntry.path; }
+            set { }
+        }
+        public override string shortname
+        {
+            get { return tahEntry.shortname; }
+            set { }
+        }
+        public override int version
+        {
+            get { return tahEntry.version; }
+            set { }
+        }
+    }
+    public class GenericZipsTahInfo : GenericTahInfo
+    {
+        private ArcsZipTahEntry tahEntry;
+        public GenericZipsTahInfo(ArcsZipTahEntry entry)
+        {
+            tahEntry = entry;
+        }
+        public GenericZipsTahInfo(GenericTahInfo info)
+        {
+            tahEntry = new ArcsZipTahEntry();
+            tahEntry.id=info.id;
+            tahEntry.path=info.path;
+            tahEntry.shortname=info.shortname;
+            tahEntry.version=info.version;
+            tahEntry.zipid = info.zipid;
+        }
+        public override int id
+        {
+            get { return tahEntry.id; }
+            set { }
+        }
+        public override int zipid
+        {
+            get { return tahEntry.zipid; }
+            set { }
+        }
+        public override string path
+        {
+            get { return tahEntry.path; }
+            set { }
+        }
+        public override string shortname
+        {
+            get { return tahEntry.shortname; }
+            set { }
+        }
+        public override int version
+        {
+            get { return tahEntry.version; }
+            set { }
+        }
+    }
+}
