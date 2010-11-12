83cb612b4703eecfa2c2d782e451dcd1ade5fa4d
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2010-11-12 19:23:36 +0000

    added TMOFlip

---
+++ trunk/tsoview/TMOFlip/Program.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using TDCG;
+
+namespace TMOFlip
+{
+class Program
+{
+    public static string GetSwapNodesPath()
+    {
+        return Path.Combine(Application.StartupPath, @"swapnodes.txt");
+    }
+
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("TMOFlip.exe <tmo file>");
+            return;
+        }
+        string source_file = args[0];
+
+        TMOFile tmo = new TMOFile();
+        tmo.Load(source_file);
+
+        Dictionary<string, TMONode> nodemap;
+        nodemap = new Dictionary<string, TMONode>();
+
+        foreach (TMONode node in tmo.nodes)
+        {
+            nodemap.Add(node.Name, node);
+        }
+
+        char[] delim = { ' ' };
+        using (StreamReader source = new StreamReader(File.OpenRead(GetSwapNodesPath())))
+        {
+            string line;
+            while ((line = source.ReadLine()) != null)
+            {
+                string[] tokens = line.Split(delim);
+                Debug.Assert(tokens.Length == 3, "tokens length should be 3");
+                string op = tokens[0];
+                if (op == "swap")
+                {
+                    string lnode_name = tokens[1];
+                    string rnode_name = tokens[2];
+                    int lnode_id = nodemap[lnode_name].ID;
+                    int rnode_id = nodemap[rnode_name].ID;
+
+                    foreach (TMOFrame frame in tmo.frames)
+                    {
+                        TMOMat lmat = frame.matrices[lnode_id];
+                        TMOMat rmat = frame.matrices[rnode_id];
+                        FlipMatrix(ref lmat.m);
+                        FlipMatrix(ref rmat.m);
+                        frame.matrices[nodemap[lnode_name].ID] = rmat;
+                        frame.matrices[nodemap[rnode_name].ID] = lmat;
+                    }
+                }
+            }
+        }
+
+        string dest_file = Path.GetFileNameWithoutExtension(source_file) + @".new.tmo";
+        Console.WriteLine("Save File: " + dest_file);
+        tmo.Save(dest_file);
+    }
+
+    static void FlipMatrix(ref Matrix m)
+    {
+        //y‰ñ“]
+        m.M31 = -m.M31;
+        m.M13 = -m.M13;
+
+        //z‰ñ“]
+        m.M21 = -m.M21;
+        m.M12 = -m.M12;
+
+        //xˆÚ“®
+        m.M41 = -m.M41;
+    }
+}
+}
---
+++ trunk/tsoview/build-TMOFlip.bat
@@ -0,0 +1,1 @@
+csc /out:build\TMOFlip.exe @tsoview.rsp TMOFlip\Program.cs
