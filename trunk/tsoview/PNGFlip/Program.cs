f354026fa69aff46d6095e56174d17839254571a
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2010-11-13 16:35:12 +0000

    added PNGFlip

---
+++ trunk/tsoview/PNGFlip/Program.cs
@@ -0,0 +1,272 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
+using TDCG;
+
+namespace PNGFlip
+{
+    public class TSOData
+    {
+        internal uint opt1;
+        internal byte[] ftso;
+    }
+
+    public class TSOFigure
+    {
+        internal List<TSOData> TSOList = new List<TSOData>();
+        internal TMOFile tmo = null;
+
+        internal byte[] lgta;
+        internal byte[] figu;
+    }
+
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            if (args.Length < 1)
+            {
+                System.Console.WriteLine("Usage: PNGFlip <source png>");
+                return;
+            }
+
+            string source_file = args[0];
+
+            Program program = new Program();
+            program.Process(source_file);
+        }
+
+        internal List<TSOFigure> TSOFigureList = new List<TSOFigure>();
+
+        byte[] cami;
+
+        public bool Process(string source_file)
+        {
+            List<TSOFigure> fig_list = new List<TSOFigure>();
+
+            Console.WriteLine("Load File: " + source_file);
+            PNGFile source = new PNGFile();
+            string source_type = "";
+
+            try
+            {
+                TSOFigure fig = null;
+                TMOFile tmo = null;
+
+                source.Hsav += delegate(string type)
+                {
+                    source_type = type;
+
+                    fig = new TSOFigure();
+                    fig_list.Add(fig);
+                };
+                source.Pose += delegate(string type)
+                {
+                    source_type = type;
+                };
+                source.Scne += delegate(string type)
+                {
+                    source_type = type;
+                };
+                source.Cami += delegate(Stream dest, int extract_length)
+                {
+                    cami = new byte[extract_length];
+                    dest.Read(cami, 0, extract_length);
+                };
+                source.Lgta += delegate(Stream dest, int extract_length)
+                {
+                    byte[] lgta = new byte[extract_length];
+                    dest.Read(lgta, 0, extract_length);
+
+                    fig = new TSOFigure();
+                    fig.lgta = lgta;
+                    fig_list.Add(fig);
+                };
+                source.Ftmo += delegate(Stream dest, int extract_length)
+                {
+                    tmo = new TMOFile();
+                    tmo.Load(dest);
+                    fig.tmo = tmo;
+                };
+                source.Figu += delegate(Stream dest, int extract_length)
+                {
+                    byte[] figu = new byte[extract_length];
+                    dest.Read(figu, 0, extract_length);
+
+                    fig.figu = figu;
+                };
+                source.Ftso += delegate(Stream dest, int extract_length, byte[] opt1)
+                {
+                    byte[] ftso = new byte[extract_length];
+                    dest.Read(ftso, 0, extract_length);
+
+                    TSOData tso = new TSOData();
+                    tso.opt1 = BitConverter.ToUInt32(opt1, 0);
+                    tso.ftso = ftso;
+                    fig.TSOList.Add(tso);
+                };
+
+                source.Load(source_file);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+            foreach (TSOFigure fig in fig_list)
+            {
+                TSOFigureList.Add(fig);
+            }
+
+            foreach (TSOFigure fig in TSOFigureList)
+                if (fig.tmo != null)
+                if (fig.tmo.nodes[0].Path == "|W_Hips")
+                {
+                    Flip(fig.tmo);
+                }
+
+            string dest_path = Path.GetDirectoryName(source_file);
+            string dest_file = Path.GetFileNameWithoutExtension(source_file) + @".new.png";
+            Console.WriteLine("Save File: " + dest_file);
+            source.WriteTaOb += delegate(BinaryWriter bw)
+            {
+                PNGWriter pw = new PNGWriter(bw);
+                switch (source_type)
+                {
+                case "HSAV":
+                    WriteHsav(pw);
+                    break;
+                case "POSE":
+                    WritePose(pw);
+                    break;
+                case "SCNE":
+                    WriteScne(pw);
+                    break;
+                }
+            };
+            source.Save(dest_file);
+
+            return true;
+        }
+
+        protected void WriteHsav(PNGWriter pw)
+        {
+            pw.WriteTDCG();
+            pw.WriteHSAV();
+            foreach (TSOFigure fig in TSOFigureList)
+                foreach (TSOData tso in fig.TSOList)
+                    pw.WriteFTSO(tso.opt1, tso.ftso);
+        }
+
+        protected void WritePose(PNGWriter pw)
+        {
+            pw.WriteTDCG();
+            pw.WritePOSE();
+            pw.WriteCAMI(cami);
+            foreach (TSOFigure fig in TSOFigureList)
+            {
+                pw.WriteLGTA(fig.lgta);
+                pw.WriteFTMO(fig.tmo);
+            }
+        }
+
+        protected void WriteScne(PNGWriter pw)
+        {
+            pw.WriteTDCG();
+            pw.WriteSCNE(FigureCount());
+            pw.WriteCAMI(cami);
+            foreach (TSOFigure fig in TSOFigureList)
+            {
+                pw.WriteLGTA(fig.lgta);
+                pw.WriteFTMO(fig.tmo);
+                pw.WriteFIGU(fig.figu);
+                foreach (TSOData tso in fig.TSOList)
+                    pw.WriteFTSO(tso.opt1, tso.ftso);
+            }
+        }
+
+        protected int FigureCount()
+        {
+            return TSOFigureList.Count;
+        }
+
+    public static string GetFlipNodesPath()
+    {
+        return Path.Combine(Application.StartupPath, @"flipnodes.txt");
+    }
+
+    void Flip(TMOFile tmo)
+    {
+        Dictionary<string, TMONode> nodemap;
+        nodemap = new Dictionary<string, TMONode>();
+
+        foreach (TMONode node in tmo.nodes)
+        {
+            nodemap.Add(node.Name, node);
+        }
+
+        char[] delim = { ' ' };
+        using (StreamReader source = new StreamReader(File.OpenRead(GetFlipNodesPath())))
+        {
+            string line;
+            while ((line = source.ReadLine()) != null)
+            {
+                string[] tokens = line.Split(delim);
+                string op = tokens[0];
+                if (op == "flip")
+                {
+                    Debug.Assert(tokens.Length == 2, "tokens length should be 2");
+                    string cnode_name = tokens[1];
+                    int cnode_id = nodemap[cnode_name].ID;
+
+                    foreach (TMOFrame frame in tmo.frames)
+                    {
+                        TMOMat cmat = frame.matrices[cnode_id];
+                        FlipMatrix(ref cmat.m);
+                    }
+                }
+                else
+                if (op == "swap")
+                {
+                    Debug.Assert(tokens.Length == 3, "tokens length should be 3");
+                    string lnode_name = tokens[1];
+                    string rnode_name = tokens[2];
+                    int lnode_id = nodemap[lnode_name].ID;
+                    int rnode_id = nodemap[rnode_name].ID;
+
+                    foreach (TMOFrame frame in tmo.frames)
+                    {
+                        TMOMat lmat = frame.matrices[lnode_id];
+                        TMOMat rmat = frame.matrices[rnode_id];
+                        FlipMatrix(ref lmat.m);
+                        FlipMatrix(ref rmat.m);
+                        frame.matrices[lnode_id] = rmat;
+                        frame.matrices[rnode_id] = lmat;
+                    }
+                }
+            }
+        }
+    }
+
+    static void FlipMatrix(ref Matrix m)
+    {
+        //y‰ñ“]
+        m.M31 = -m.M31;
+        m.M13 = -m.M13;
+
+        //z‰ñ“]
+        m.M21 = -m.M21;
+        m.M12 = -m.M12;
+
+        //xˆÚ“®
+        m.M41 = -m.M41;
+    }
+    }
+}
---
+++ trunk/tsoview/debug-PNGFlip.bat
@@ -0,0 +1,1 @@
+csc /debug /d:DEBUG /out:debug\PNGFlip.exe @pngfile.rsp PNGFlip\Program.cs
