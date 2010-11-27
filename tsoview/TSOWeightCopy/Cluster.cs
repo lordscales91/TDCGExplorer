7d43ae141fe03686d7bc836671976df15f1b0adb
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2010-11-27 08:34:04 +0000

    revised

---
+++ tsoview/TSOWeightCopy/Cluster.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using TDCG;
+
+namespace TSOWeightCopy
+{
+    /// 頂点探索クラスタ
+    public class Cluster
+    {
+        Vector3 min;
+        Vector3 max;
+        UniqueCell[, ,] cells;
+        int xlen;
+        int ylen;
+        int zlen;
+
+        public Cluster(Vector3 min, Vector3 max)
+        {
+            this.min = min;
+            this.max = max;
+            this.xlen = (int)Math.Floor(max.X + 0.5f) - (int)Math.Floor(min.X + 0.5f) + 1;
+            this.ylen = (int)Math.Floor(max.Y + 0.5f) - (int)Math.Floor(min.Y + 0.5f) + 1;
+            this.zlen = (int)Math.Floor(max.Z + 0.5f) - (int)Math.Floor(min.Z + 0.5f) + 1;
+            this.cells = new UniqueCell[xlen, ylen, zlen];
+        }
+
+        /// 指定x座標にある頂点を格納するセルのx値を得ます。
+        public int GetX(float x)
+        {
+            return (int)Math.Floor(x + 0.5f) - (int)Math.Floor(min.X + 0.5f);
+        }
+
+        /// 指定y座標にある頂点を格納するセルのy値を得ます。
+        public int GetY(float y)
+        {
+            return (int)Math.Floor(y + 0.5f) - (int)Math.Floor(min.Y + 0.5f);
+        }
+
+        /// 指定z座標にある頂点を格納するセルのz値を得ます。
+        public int GetZ(float z)
+        {
+            return (int)Math.Floor(z + 0.5f) - (int)Math.Floor(min.Z + 0.5f);
+        }
+
+        /// 指定座標にあるセルを得ます。
+        public UniqueCell GetCell(int x, int y, int z)
+        {
+            if (cells[x, y, z] == null)
+                cells[x, y, z] = new UniqueCell(x, y, z, x == GetX(0.0f));
+            return cells[x, y, z];
+        }
+
+        /// 頂点を追加します。
+        public void Push(Vertex a, TSOSubMesh sub)
+        {
+            int x = GetX(a.position.X);
+            int y = GetY(a.position.Y);
+            int z = GetZ(a.position.Z);
+            UniqueCell cell = GetCell(x, y, z);
+            cell.Push(a, sub);
+        }
+
+        public void Dump()
+        {
+            foreach (UniqueCell cell in cells)
+                if (cell != null)
+                    cell.Dump();
+        }
+
+        /// 同一視頂点リスト
+        public List<UniqueVertex> vertices
+        {
+            get
+            {
+                List<UniqueVertex> ary = new List<UniqueVertex>();
+                foreach (UniqueCell cell in cells)
+                    if (cell != null)
+                        ary.AddRange(cell.vertices);
+                return ary;
+            }
+        }
+
+        /// 対称位置にあるセルのx値を得ます。
+        public int GetOppositeX(int x)
+        {
+            int xend = xlen - 1;
+            return xend - x;
+        }
+
+        /// 対称位置にあるセルを保持します。
+        public void AssignOppositeCells()
+        {
+            foreach (UniqueCell cell in cells)
+                if (cell != null)
+                {
+                    int x = GetOppositeX(cell.X);
+                    int y = cell.Y;
+                    int z = cell.Z;
+                    cell.opposite_cell = GetCell(x, y, z);
+                }
+        }
+
+        /// 対称位置にある同一視頂点を保持します。
+        public void AssignOppositeVertices()
+        {
+            foreach (UniqueCell cell in cells)
+                if (cell != null)
+                    cell.AssignOppositeVertices();
+        }
+
+        /// 対称位置にある同一視頂点のスキンウェイトを複写します。
+        public void CopyOppositeWeights()
+        {
+            int x = GetX(0.0f);
+            foreach (UniqueCell cell in cells)
+                if (cell != null)
+                {
+                    if (cell.X > x)
+                        continue;
+
+                    cell.CopyOppositeWeights();
+                }
+        }
+    }
+}
---
+++ tsoview/TSOWeightCopy/UniqueCell.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using TDCG;
+
+namespace TSOWeightCopy
+{
+    /// 頂点探索クラスタに格納するセル
+    public class UniqueCell
+    {
+        public static float LengthSq(Vector3 a, Vector3 b)
+        {
+            float dx = b.X - a.X;
+            float dy = b.Y - a.Y;
+            float dz = b.Z - a.Z;
+            return dx * dx + dy * dy + dz * dz;
+        }
+
+        int x;
+        int y;
+        int z;
+        bool contains_zero_x;
+
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+        public int Z { get { return z; } }
+        public bool ContainsZeroX { get { return contains_zero_x; } }
+
+        public List<UniqueVertex> vertices;
+        public UniqueCell opposite_cell;
+
+        public UniqueCell(int x, int y, int z, bool contains_zero_x)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.contains_zero_x = contains_zero_x;
+            this.vertices = new List<UniqueVertex>();
+            this.opposite_cell = null;
+        }
+
+        /// 頂点を追加します。
+        public void Push(Vertex a, TSOSubMesh sub)
+        {
+            bool found = false;
+            foreach (UniqueVertex v in vertices)
+            {
+                if (LengthSq(a.position, v.position) < float.Epsilon)
+                {
+                    v.Push(a, sub);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                vertices.Add(new UniqueVertex(a, sub));
+        }
+
+        public override string ToString()
+        {
+            return string.Format("x:{0} y:{1} z:{2} #v:{3}", x, y, z, vertices.Count);
+        }
+
+        public void Dump()
+        {
+            Console.WriteLine(this);
+            if (vertices.Count > 0)
+            {
+                UniqueVertex v = vertices[0];
+                v.Dump();
+            }
+        }
+
+        /// 指定位置に最も近い同一視頂点を見つけます。
+        public UniqueVertex FindVertexAt(Vector3 position)
+        {
+            float min_len_sq = 10.0f;
+            UniqueVertex found = null;
+            foreach (UniqueVertex v in vertices)
+            {
+                float len_sq = LengthSq(position, v.position);
+                if (min_len_sq > len_sq)
+                {
+                    min_len_sq = len_sq;
+                    found = v;
+                }
+            }
+            return found;
+        }
+
+        /// 対称位置にある同一視頂点を保持します。
+        public void AssignOppositeVertices()
+        {
+            if (contains_zero_x)
+            {
+                foreach (UniqueVertex v in vertices)
+                    v.opposite_vertex = Math.Abs(v.position.X) < 1.0e-4f ? v : opposite_cell.FindVertexAt(v.GetOppositePosition());
+            }
+            else
+            {
+                foreach (UniqueVertex v in vertices)
+                    v.opposite_vertex = opposite_cell.FindVertexAt(v.GetOppositePosition());
+            }
+        }
+
+        /// 対称位置にある同一視頂点のスキンウェイトを複写します。
+        public void CopyOppositeWeights()
+        {
+            if (contains_zero_x)
+            {
+                foreach (UniqueVertex v in vertices)
+                {
+                    if (v.position.X > -1.0e-4f)
+                        continue;
+
+                    v.CopyOppositeWeights();
+                }
+            }
+            else
+            {
+                foreach (UniqueVertex v in vertices)
+                    v.CopyOppositeWeights();
+            }
+        }
+    }
+}
