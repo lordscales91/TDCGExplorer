8d484f48b7088747f4fedfc482508c7752a7e011
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2010-11-27 09:11:33 +0000

    oppnode_idmap

--- tsoview/TSOWeightCopy/Program.cs
+++ tsoview/TSOWeightCopy/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
 using Microsoft.DirectX;
 using Microsoft.DirectX.Direct3D;
 using TDCG;
@@ -8,10 +11,55 @@ namespace TSOWeightCopy
 {
     class Program
     {
+        public static string GetFlipNodesPath()
+        {
+            return Path.Combine(Application.StartupPath, @"flipnodes.txt");
+        }
+
         static void Main(string[] args)
         {
             TSOFile tso = new TSOFile();
             tso.Load(@"base/data/model/N001BODY_A00.tso");
+
+            Dictionary<string, TSONode> nodemap = new Dictionary<string, TSONode>();
+
+            foreach (TSONode node in tso.nodes)
+                nodemap.Add(node.Name, node);
+
+            Dictionary<int, int> oppnode_idmap = new Dictionary<int, int>();
+
+            char[] delim = { ' ' };
+            using (StreamReader source = new StreamReader(File.OpenRead(GetFlipNodesPath())))
+            {
+                string line;
+                while ((line = source.ReadLine()) != null)
+                {
+                    string[] tokens = line.Split(delim);
+                    string op = tokens[0];
+                    if (op == "flip")
+                    {
+                        Debug.Assert(tokens.Length == 2, "tokens length should be 2: " + line);
+                        string cnode_name = tokens[1];
+                        int cnode_id = nodemap[cnode_name].ID;
+
+                        oppnode_idmap[cnode_id] = cnode_id;
+                    }
+                    else
+                    if (op == "swap")
+                    {
+                        Debug.Assert(tokens.Length == 3, "tokens length should be 3: " + line);
+                        string lnode_name = tokens[1];
+                        string rnode_name = tokens[2];
+                        int lnode_id = nodemap[lnode_name].ID;
+                        int rnode_id = nodemap[rnode_name].ID;
+
+                        oppnode_idmap[lnode_id] = rnode_id;
+                        oppnode_idmap[rnode_id] = lnode_id;
+                    }
+                }
+            }
+
+            UniqueVertex.oppnode_idmap = oppnode_idmap;
 
             TSOMesh found_mesh = null;
             foreach (TSOMesh mesh in tso.meshes)
@@ -65,6 +113,8 @@ namespace TSOWeightCopy
             cluster.AssignOppositeVertices();
             //cluster.Dump();
             cluster.CopyOppositeWeights();
+
+            tso.Save(@"out.tso");
         }
     }
 }
