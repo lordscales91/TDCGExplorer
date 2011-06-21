10c613edb2263352a39c25400aa0fc1e0a4d1b1b
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2011-06-21 16:04:31 +0000

    load tso

--- trunk/tsoview/tso2pmx/Program.cs
+++ trunk/tsoview/tso2pmx/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TDCG;
 
 namespace tso2pmx
 {
@@ -9,8 +10,31 @@ namespace tso2pmx
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                System.Console.WriteLine("Usage: tso2pmx <source tso>");
+                return;
+            }
+
+            string source_file = args[0];
+
+            Program program = new Program();
+            program.Process(source_file);
+        }
+
+        public bool Process(string source_file)
+        {
+            TSOFile tso;
+            tso = new TSOFile();
+            tso.Load(source_file);
+
             PmxFile pmx = new PmxFile();
-            pmx.Save("out.pmx");
+
+            string dest_file = "out.pmx";
+            Console.WriteLine("Save File: " + dest_file);
+            pmx.Save(dest_file);
+
+            return true;
         }
     }
 }
