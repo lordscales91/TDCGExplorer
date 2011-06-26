d50d8f2c463e812aece4cbdf90dd4102026a413a
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2011-06-26 16:33:24 +0000

    create branch for Tso2Pmd pmd

--- marimo/db/migrate/20090509034432_add_locked_to_tag.rb
+++
@@ -1,9 +0,0 @@
-class AddLockedToTag < ActiveRecord::Migration
-  def self.up
-    add_column :tags, :locked, :boolean, :null => false, :default => false
-  end
-
-  def self.down
-    remove_column :tags, :locked
-  end
-end
