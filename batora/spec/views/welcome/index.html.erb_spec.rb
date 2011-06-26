1583efa05ecfc5517583327b235e47bff46bb61f
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2011-06-26 15:04:09 +0000

    create branch for Tso2Pmd pmx

--- batora/spec/views/welcome/index.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
-
-describe "/welcome/index" do
-
-  it "should render the template" do
-    render
-  end
-end
--- tsoview/lib/unified_position_texcoord_vertex.rb
+++
@@ -1,8 +0,0 @@
-class UnifiedPositionTexcoordVertex < TDCG::Vertex
-  def eql?(o)
-    position == o.position && u == o.u && v == o.v
-  end
-  def hash
-    position.hash ^ u.hash ^ v.hash
-  end
-end
