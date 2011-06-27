67efc03f610db7090e0e859f7ec38a0d6caa6d8a
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2011-06-27 13:47:10 +0000

    fixed PhysObTemplate

--- Share/PhysObTemplate/Kami_Default.cs
+++ Share/PhysObTemplate/Kami_Default.cs
@@ -8,11 +8,6 @@ using TDCG;
 using TDCGUtils;
 using Tso2Pmd;
 
-using jp.nyatla.nymmd.cs.types;
-using jp.nyatla.nymmd.cs.core;
-using jp.nyatla.nymmd.cs.struct_type;
-using jp.nyatla.nymmd.cs.struct_type.pmd;
-
 namespace TDCG.PhysObTemplate
 {
     public class Kami_Default : IPhysObTemplate
@@ -37,80 +32,80 @@ namespace TDCG.PhysObTemplate
             SetParameterEnd(phys_list.GetBodyListByName(".髪.*先"));
             SetParameter(phys_list.GetJointListByName(".髪.*"));
 
-            phys_list.GetBodyByName("中髪後１").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("中髪後２").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetJointByName("中髪後２-中髪後３").constrain_rot_1.x = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
-            phys_list.GetJointByName("中髪後２-中髪後３").constrain_rot_2.x = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
-            phys_list.GetJointByName("中髪後３-中髪後４").constrain_rot_1.x = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
-            phys_list.GetJointByName("中髪後３-中髪後４").constrain_rot_2.x = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
+            phys_list.GetBodyByName("中髪後１").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("中髪後２").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetJointByName("中髪後２-中髪後３").rotation_min.X = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
+            phys_list.GetJointByName("中髪後２-中髪後３").rotation_max.X = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
+            phys_list.GetJointByName("中髪後３-中髪後４").rotation_min.X = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
+            phys_list.GetJointByName("中髪後３-中髪後４").rotation_max.X = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
 
-            phys_list.GetBodyByName("左髪横１").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("左髪横２").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("左髪横３").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("左髪横１").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("左髪横２").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("左髪横３").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
 
-            phys_list.GetBodyByName("右髪横１").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("右髪横２").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("右髪横３").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("右髪横１").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("右髪横２").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("右髪横３").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
 
-            phys_list.GetBodyByName("左髪後１").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("左髪後２").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetJointByName("左髪後２-左髪後３").constrain_rot_1.x = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
-            phys_list.GetJointByName("左髪後２-左髪後３").constrain_rot_2.x = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
-            phys_list.GetJointByName("左髪後３-左髪後４").constrain_rot_1.x = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
-            phys_list.GetJointByName("左髪後３-左髪後４").constrain_rot_2.x = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
+            phys_list.GetBodyByName("左髪後１").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("左髪後２").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetJointByName("左髪後２-左髪後３").rotation_min.X = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
+            phys_list.GetJointByName("左髪後２-左髪後３").rotation_max.X = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
+            phys_list.GetJointByName("左髪後３-左髪後４").rotation_min.X = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
+            phys_list.GetJointByName("左髪後３-左髪後４").rotation_max.X = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
 
-            phys_list.GetBodyByName("右髪後１").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("右髪後２").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetJointByName("右髪後２-右髪後３").constrain_rot_1.x = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
-            phys_list.GetJointByName("右髪後２-右髪後３").constrain_rot_2.x = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
-            phys_list.GetJointByName("右髪後３-右髪後４").constrain_rot_1.x = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
-            phys_list.GetJointByName("右髪後３-右髪後４").constrain_rot_2.x = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
+            phys_list.GetBodyByName("右髪後１").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("右髪後２").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetJointByName("右髪後２-右髪後３").rotation_min.X = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
+            phys_list.GetJointByName("右髪後２-右髪後３").rotation_max.X = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
+            phys_list.GetJointByName("右髪後３-右髪後４").rotation_min.X = (float)((-5.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
+            phys_list.GetJointByName("右髪後３-右髪後４").rotation_max.X = (float)((30.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
 
-            phys_list.GetBodyByName("左髪前１").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("左髪前２").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("左髪前３").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("左髪前先").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("左髪前１").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("左髪前２").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("左髪前３").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("左髪前先").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
 
-            phys_list.GetBodyByName("右髪前１").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("右髪前２").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("右髪前３").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
-            phys_list.GetBodyByName("右髪前先").rigidbody_type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("右髪前１").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("右髪前２").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("右髪前３").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+            phys_list.GetBodyByName("右髪前先").type = 0; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
         }
 
-        private void SetParameter(List<PMD_RigidBody> body_list)
+        private void SetParameter(List<PMD_RBody> body_list)
         {
-            foreach (PMD_RigidBody body in body_list)
+            foreach (PMD_RBody body in body_list)
             {
-                body.rigidbody_group_index = 2; // 諸データ：グループ // 00
-                body.rigidbody_group_target = 1; // 諸データ：グループ：対象 // 0xFFFFとの差 // 38 FE
-                body.shape_type = 2; // 形状：タイプ(0:球、1:箱、2:カプセル) // 00 // 球
-                body.shape_w = 0.4f; // 形状：半径(幅) // CD CC CC 3F // 1.6
-                body.shape_h *= 0.6f; // 形状：高さ // CD CC CC 3D // 0.1
+                body.group_id = 2; // 諸データ：グループ // 00
+                body.group_non_collision = 1; // 諸データ：グループ：対象 // 0xFFFFとの差 // 38 FE
+                body.shape_id = 2; // 形状：タイプ(0:球、1:箱、2:カプセル) // 00 // 球
+                body.size.X = 0.4f; // 形状：半径(幅) // CD CC CC 3F // 1.6
+                body.size.Y *= 0.6f; // 形状：高さ // CD CC CC 3D // 0.1
 
-                body.rigidbody_weight = 0.5f; // 諸データ：質量 // 00 00 80 3F // 1.0
-                body.rigidbody_pos_dim = 0.8f; // 諸データ：移動減 // 00 00 00 00
-                body.rigidbody_rot_dim = 0.8f; // 諸データ：回転減 // 00 00 00 00
-                body.rigidbody_recoil = 0.0f; // 諸データ：反発力 // 00 00 00 00
-                body.rigidbody_friction = 0.8f; // 諸データ：摩擦力 // 00 00 00 00
-                body.rigidbody_type = 1; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+                body.weight = 0.5f; // 諸データ：質量 // 00 00 80 3F // 1.0
+                body.position_dim = 0.8f; // 諸データ：移動減 // 00 00 00 00
+                body.rotation_dim = 0.8f; // 諸データ：回転減 // 00 00 00 00
+                body.recoil = 0.0f; // 諸データ：反発力 // 00 00 00 00
+                body.friction = 0.8f; // 諸データ：摩擦力 // 00 00 00 00
+                body.type = 1; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
             }
         }
 
-        private void SetParameterEnd(List<PMD_RigidBody> body_list)
+        private void SetParameterEnd(List<PMD_RBody> body_list)
         {
-            foreach (PMD_RigidBody body in body_list)
+            foreach (PMD_RBody body in body_list)
             {
-                body.rigidbody_group_index = 2; // 諸データ：グループ // 00
-                body.rigidbody_group_target = 1; // 諸データ：グループ：対象 // 0xFFFFとの差 // 38 FE
-                body.shape_type = 0; // 形状：タイプ(0:球、1:箱、2:カプセル) // 00 // 球
-                body.shape_w = 0.3f; // 形状：半径(幅) // CD CC CC 3F // 1.6
+                body.group_id = 2; // 諸データ：グループ // 00
+                body.group_non_collision = 1; // 諸データ：グループ：対象 // 0xFFFFとの差 // 38 FE
+                body.shape_id = 0; // 形状：タイプ(0:球、1:箱、2:カプセル) // 00 // 球
+                body.size.X = 0.3f; // 形状：半径(幅) // CD CC CC 3F // 1.6
 
-                body.rigidbody_weight = 1.0f; // 諸データ：質量 // 00 00 80 3F // 1.0
-                body.rigidbody_pos_dim = 0.8f; // 諸データ：移動減 // 00 00 00 00
-                body.rigidbody_rot_dim = 0.8f; // 諸データ：回転減 // 00 00 00 00
-                body.rigidbody_recoil = 0.0f; // 諸データ：反発力 // 00 00 00 00
-                body.rigidbody_friction = 0.8f; // 諸データ：摩擦力 // 00 00 00 00
-                body.rigidbody_type = 1; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
+                body.weight = 1.0f; // 諸データ：質量 // 00 00 80 3F // 1.0
+                body.position_dim = 0.8f; // 諸データ：移動減 // 00 00 00 00
+                body.rotation_dim = 0.8f; // 諸データ：回転減 // 00 00 00 00
+                body.recoil = 0.0f; // 諸データ：反発力 // 00 00 00 00
+                body.friction = 0.8f; // 諸データ：摩擦力 // 00 00 00 00
+                body.type = 1; // 諸データ：タイプ(0:Bone追従、1:物理演算、2:物理演算(Bone位置合せ)) // 00 // Bone追従
             }
         }
 
@@ -118,13 +113,11 @@ namespace TDCG.PhysObTemplate
         {
             foreach (PMD_Joint joint in joint_list)
             {
-                joint.constrain_rot_1.x = (float)((-20.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
-                joint.constrain_rot_2.x = (float)((20.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
-                joint.constrain_rot_1.z = (float)((-20.0 / 180.0) * Math.PI); // 制限：回転1(rad(x), rad(y), rad(z))
-                joint.constrain_rot_2.z = (float)((20.0 / 180.0) * Math.PI); // 制限：回転2(rad(x), rad(y), rad(z))
-                joint.spring_rot.x = 10.0f; // ばね：回転(rad(x), rad(y), rad(z))
-                joint.spring_rot.y = 10.0f; // ばね：回転(rad(x), rad(y), rad(z))
-                joint.spring_rot.z = 10.0f; // ばね：回転(rad(x), rad(y), rad(z))
+                joint.rotation_min.X = Geometry.DegreeToRadian(-20.0f);
+                joint.rotation_max.X = Geometry.DegreeToRadian(+20.0f);
+                joint.rotation_min.Z = Geometry.DegreeToRadian(-20.0f);
+                joint.rotation_max.Z = Geometry.DegreeToRadian(+20.0f);
+                joint.spring_rotation = new Vector3(10.0f, 10.0f, 10.0f);
             }
         }
     }
