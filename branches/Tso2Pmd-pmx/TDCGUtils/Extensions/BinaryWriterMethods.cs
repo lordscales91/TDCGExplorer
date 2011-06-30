c59a30eb372ade83f991d706081323d8bb2102ec
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2011-06-30 01:16:54 +0000

    added

--- branches/Tso2Pmd-pmx/TDCGUtils/Extensions/BinaryWriterMethods.cs
+++ branches/Tso2Pmd-pmx/TDCGUtils/Extensions/BinaryWriterMethods.cs
@@ -12,6 +12,24 @@ namespace TDCGUtils.Extensions
     /// </summary>
     public static class BinaryWriterMethods
     {
+        public static void WriteCString(this BinaryWriter bw, String str, int i_length)
+        {
+            if (str != null)
+            {
+                bw.Write(Encoding.GetEncoding(932).GetBytes(str)); // Shift JISとしてbyte配列に変換
+                if (i_length > Encoding.GetEncoding(932).GetBytes(str).Length)
+                    bw.Write(Convert.ToByte("00", 16)); // 文字列終端の記号
+                for (int i = 0; i < (i_length - 1 - Encoding.GetEncoding(932).GetBytes(str).Length); i++)
+                    bw.Write(Convert.ToByte("FD", 16)); // ストリームの位置を合わせるため、意味のないByteを書き込む
+            }
+            else
+            {
+                bw.Write(Convert.ToByte("00", 16)); // 文字列終端の記号
+                for (int i = 0; i < (i_length - 1); i++)
+                    bw.Write(Convert.ToByte("FD", 16)); // ストリームの位置を合わせるため、意味のないByteを書き込む
+            }
+        }
+
         /// <summary>
         /// 指定ライタに文字byte長と文字列を書き出します。
         /// </summary>
