8ac94f5a4246792f1dcc714c69cb0927e0663b28
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2011-08-07 21:44:16 +0000

    renamed

--- trunk/tsoview/TSODeform/Properties/AssemblyInfo.cs
+++ trunk/tsoview/TSODeform/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("TSOSkin")]
+[assembly: AssemblyTitle("TSODeform")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TSOSkin")]
+[assembly: AssemblyProduct("TSODeform")]
 [assembly: AssemblyCopyright("Copyright c  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
