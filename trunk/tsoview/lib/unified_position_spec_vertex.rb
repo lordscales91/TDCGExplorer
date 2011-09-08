c0db716cedacc6865b0a663fa8961c668489b184
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2011-09-08 00:18:15 +0000

    refactored

--- trunk/tsoview/lib/unified_position_spec_vertex.rb
+++ trunk/tsoview/lib/unified_position_spec_vertex.rb
@@ -1,15 +1,15 @@
 class UnifiedPositionSpecVertex < TDCG::Vertex
   attr :spec
-  def initialize(a, sub)
+  def initialize(a, bone_indices, spec)
     self.position = a.position
     self.normal = a.normal
     self.u = a.u
     self.v = a.v
     self.skin_weights = System::Array[TDCG::SkinWeight].new(4)
     4.times do |i|
-      self.skin_weights[i] = TDCG::SkinWeight.new(sub.bone_indices[a.skin_weights[i].bone_index], a.skin_weights[i].weight)
+      self.skin_weights[i] = TDCG::SkinWeight.new(bone_indices[a.skin_weights[i].bone_index], a.skin_weights[i].weight)
     end
-    @spec = sub.spec
+    @spec = spec
   end
   def eql?(o)
     position == o.position && spec == o.spec
