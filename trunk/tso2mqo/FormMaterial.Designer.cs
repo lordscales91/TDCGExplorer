784f41e889a7b29928d204493cdd2fe3708703b8
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2011-09-17 01:10:50 +0000

    imported

---
+++ trunk/tso2mqo/FormMaterial.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Tso2MqoGui
+{
+    partial class FormMaterial
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvMaterials = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pgMaterial = new System.Windows.Forms.PropertyGrid();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.bOk = new System.Windows.Forms.Button();
+            this.bCancel = new System.Windows.Forms.Button();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvMaterials
+            // 
+            this.lvMaterials.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvMaterials.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvMaterials.FullRowSelect = true;
+            this.lvMaterials.HideSelection = false;
+            this.lvMaterials.Location = new System.Drawing.Point(0, 0);
+            this.lvMaterials.Name = "lvMaterials";
+            this.lvMaterials.Size = new System.Drawing.Size(772, 120);
+            this.lvMaterials.TabIndex = 0;
+            this.lvMaterials.UseCompatibleStateImageBehavior = false;
+            this.lvMaterials.View = System.Windows.Forms.View.Details;
+            this.lvMaterials.SelectedIndexChanged += new System.EventHandler(this.lvMaterials_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "マテリアル名";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "カラーテクスチャ";
+            this.columnHeader2.Width = 216;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "シェーディングテクスチャ";
+            this.columnHeader3.Width = 216;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "シェーダー設定";
+            this.columnHeader4.Width = 216;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(531, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "以下のマテリアルの情報を確認して、必要な場合、修正してください。\r\nファイル名が空白になっている部分に正しいファイルを設定しない場合、正しくエクスポートされない場" +
+                "合があります。";
+            // 
+            // pgMaterial
+            // 
+            this.pgMaterial.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pgMaterial.Location = new System.Drawing.Point(0, 0);
+            this.pgMaterial.Name = "pgMaterial";
+            this.pgMaterial.Size = new System.Drawing.Size(772, 211);
+            this.pgMaterial.TabIndex = 2;
+            this.pgMaterial.PropertyValueChanged += new System.Windows.Forms.PropertyValueChangedEventHandler(this.pgMaterial_PropertyValueChanged);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 36);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lvMaterials);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pgMaterial);
+            this.splitContainer1.Size = new System.Drawing.Size(772, 335);
+            this.splitContainer1.SplitterDistance = 120;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // bOk
+            // 
+            this.bOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bOk.Location = new System.Drawing.Point(628, 377);
+            this.bOk.Name = "bOk";
+            this.bOk.Size = new System.Drawing.Size(75, 23);
+            this.bOk.TabIndex = 4;
+            this.bOk.Text = "OK";
+            this.bOk.UseVisualStyleBackColor = true;
+            this.bOk.Click += new System.EventHandler(this.bOk_Click);
+            // 
+            // bCancel
+            // 
+            this.bCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bCancel.Location = new System.Drawing.Point(709, 377);
+            this.bCancel.Name = "bCancel";
+            this.bCancel.Size = new System.Drawing.Size(75, 23);
+            this.bCancel.TabIndex = 5;
+            this.bCancel.Text = "キャンセル";
+            this.bCancel.UseVisualStyleBackColor = true;
+            this.bCancel.Click += new System.EventHandler(this.bCancel_Click);
+            // 
+            // FormMaterial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 412);
+            this.Controls.Add(this.bCancel);
+            this.Controls.Add(this.bOk);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "FormMaterial";
+            this.Text = "マテリアル設定";
+            this.Load += new System.EventHandler(this.FormMaterial_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvMaterials;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PropertyGrid pgMaterial;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button bOk;
+        private System.Windows.Forms.Button bCancel;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
