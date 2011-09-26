ec71109193b1771a2065269e07bdd61099f0322a
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2011-09-26 17:00:25 +0000

    cap2tag

--- marimo/bin/move-from-summary-to-tag.rb
+++ marimo/bin/move-from-summary-to-tag.rb
@@ -2,33 +2,16 @@
 # encoding: utf-8
 require File.dirname(__FILE__) + "/../config/environment"
 
-CAP2TAG_NAMES = {}
-
-open("arc/summary-tag-uniq-mapping.txt") do |f|
-  while line = f.gets
-    row = line.chomp.split(/\t+/)
-    caption = row.shift
-    CAP2TAG_NAMES[caption] = row
-  end
-end
-
-def caption_to_tag_names(caption)
-  CAP2TAG_NAMES[caption]
-end
-
 Arc.find(:all, :conditions => ["summary like ?", '%【%']).each do |arc|
-  arc.summary.scan(/【(.+?)】/) do |caption, |
-    tag_names = caption_to_tag_names(caption)
-    if tag_names.nil?
-      puts "#{arc.id} #{arc.code} not found tag_names. #{caption}"
-      next
-    end
-    puts "#{arc.id} #{arc.code} converted. #{tag_names.join(' ')}"
-    tag_names.each do |tag_name|
-      tag = Tag.find_or_create_by_name(tag_name)
-      tag.arc_tags.find_or_create_by_arc_id(arc.id)
-    end
-    arc.summary = arc.summary.gsub(/【(.+?)】/, '')
-    arc.save
+  tag_names = Cap2tag.summary_to_tag_names(arc.summary)
+  if tag_names.empty?
+    next
   end
+  puts "#{arc.id} #{arc.code} found tag names. #{tag_names.join(' ')}"
+  tag_names.each do |tag_name|
+    tag = Tag.find_or_create_by_name(tag_name)
+    tag.arc_tags.find_or_create_by_arc_id(arc.id)
+  end
+  arc.summary = arc.summary.gsub(/【(.+?)】/, '')
+  arc.save
 end
