0a307206ef98c24f47a31fbcbf9b7f7717af6c74
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2011-10-02 11:21:42 +0000

    uploader index.html から arcs を作成

--- trunk/marimo/bin/load-arcs.rb
+++ trunk/marimo/bin/load-arcs.rb
@@ -1,34 +1,65 @@
+#!ruby
+# encoding: utf-8
+require File.dirname(__FILE__) + "/../config/environment"
+require 'net/http'
+
 site_code = ARGV.shift || 'xpc'
-location =
-  case site_code
-  when 'xpc'
-    '3DCUSTOMNET'
-  when 'mod'
-    'futaba'
-  end
-ent = "arc/#{site_code}.txt"
-open(ent) do |f|
+
+case site_code
+when 'xpc'
+  host = "cdn.3dcustom.net"
+  root_path = "/TACuploader"
+  base_html = "upload.html"
+when 'mod'
+  host = "www.nijibox5.com"
+  root_path = "/futabacustom/mod"
+  base_html = ""
+end
+local_dir = "arc/#{site_code}"
+
+header = UserAgent.default_http_header
+response = Net::HTTP.new(host).start do |http|
+  http.get("#{root_path}/#{base_html}", header)
+end
+body = response.body
+
+base_fname = "#{local_dir}/index.html"
+open(base_fname, 'w') do |f|
+  f.puts body
+end
+
+case site_code
+when 'xpc'
+  location = '3DCUSTOMNET'
+when 'mod'
+  location = 'futaba'
+end
+
+open(base_fname, 'r') do |f|
+  scrap = Scrap.find_by_site_code(site_code)
   while line = f.gets
-    name, summary, size, date, origname, locked = line.chomp.split("\t")
-    if md = /\.(.+)\z/.match(name)
-      extname = md[1]
-      code = File.basename(name, '.' + extname)
-    else
-      extname = nil
-      code = name.dup
+    if scrap.match(line)
+      name, summary, size, date, origname, locked = scrap.row
+      if md = /\.(.+)\z/.match(name)
+        extname = md[1]
+        code = File.basename(name, '.' + extname)
+      else
+        extname = nil
+        code = name.dup
+      end
+      arc = Arc.find_by_code(code)
+      if arc
+        puts "found code: #{code}"
+        next
+      end
+      puts "arcs.create code: #{code}"
+      arc = Arc.new
+      arc.code = code
+      arc.extname = extname
+      arc.location = location
+      arc.summary = summary
+      arc.origname = origname
+      arc.save
     end
-    arc = Arc.find_by_code(code)
-    if arc
-      puts "found code: #{code}"
-      next
-    end
-    puts "arcs.create code: #{code}"
-    arc = Arc.new
-    arc.code = code
-    arc.extname = extname
-    arc.location = location
-    arc.summary = summary
-    arc.origname = origname
-    arc.save
   end
 end
