0aa38b8af3fa001513c640ffa86b6e1454caf546
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2012-07-26 15:24:31 +0000

    FaceVertex

--- trunk/tsoview/TSOSmooth/TSOFace.cs
+++ trunk/tsoview/TSOSmooth/TSOFace.cs
@@ -5,36 +5,28 @@ using System.Text;
 
 namespace TSOSmooth
 {
+    /// 面頂点
+    public struct FaceVertex
+    {
+        /// 頂点参照
+        public ushort i;
+        /// テクスチャU座標
+        public float u;
+        /// テクスチャV座標
+        public float v;
+    }
+
     /// <summary>
     /// 面
     /// </summary>
     public class TSOFace
     {
-        /// <summary>
-        /// 頂点a
-        /// </summary>
-        public readonly ushort a;
-        /// <summary>
-        /// 頂点b
-        /// </summary>
-        public readonly ushort b;
-        /// <summary>
-        /// 頂点c
-        /// </summary>
-        public readonly ushort c;
-
-        /// テクスチャU座標a
-        public readonly float ua;
-        /// テクスチャV座標a
-        public readonly float va;
-        /// テクスチャU座標b
-        public readonly float ub;
-        /// テクスチャV座標b
-        public readonly float vb;
-        /// テクスチャU座標c
-        public readonly float uc;
-        /// テクスチャV座標c
-        public readonly float vc;
+        /// 面頂点a
+        public readonly FaceVertex a;
+        /// 面頂点b
+        public readonly FaceVertex b;
+        /// 面頂点c
+        public readonly FaceVertex c;
 
         /// <summary>
         /// シェーダ設定番号
@@ -44,30 +36,35 @@ namespace TSOSmooth
         /// <summary>
         /// 面を生成します。
         /// </summary>
-        /// <param name="a">頂点a</param>
-        /// <param name="b">頂点b</param>
-        /// <param name="c">頂点c</param>
-        /// <param name="ua">テクスチャU座標a</param>
-        /// <param name="va">テクスチャV座標a</param>
-        /// <param name="ub">テクスチャU座標b</param>
-        /// <param name="vb">テクスチャV座標b</param>
-        /// <param name="uc">テクスチャU座標c</param>
-        /// <param name="vc">テクスチャV座標c</param>
+        /// <param name="a">面頂点a</param>
+        /// <param name="b">面頂点b</param>
+        /// <param name="c">面頂点c</param>
         /// <param name="spec">シェーダ設定番号</param>
-        public TSOFace(ushort a, ushort b, ushort c, float ua, float va, float ub, float vb, float uc, float vc, int spec)
+        public TSOFace(FaceVertex a, FaceVertex b, FaceVertex c, int spec)
         {
             this.a = a;
             this.b = b;
             this.c = c;
 
-            this.ua = ua;
-            this.va = va;
-            this.ub = ub;
-            this.vb = vb;
-            this.uc = uc;
-            this.vc = vc;
-
             this.spec = spec;
+        }
+
+        public IEnumerable<FaceVertex> vertices
+        {
+            get {
+                yield return a;
+                yield return b;
+                yield return c;
+            }
+        }
+
+        public IEnumerable<ushort> vert_indices
+        {
+            get {
+                yield return a.i;
+                yield return b.i;
+                yield return c.i;
+            }
         }
     }
 }
