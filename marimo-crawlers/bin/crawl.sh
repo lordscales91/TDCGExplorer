d7dfbec10e76104dcfdcdf6bfc1e20cb256ace3c
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2012-11-05 17:11:56 +0000

    create branch for tsoview-SharpDX

--- marimo-crawlers/bin/crawl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ruby bin/wpc-idx.rb
-ruby bin/xpc-idx.rb
-ruby bin/xpp-idx.rb
-
-ruby bin/hnt-idx.rb
-ruby bin/mod-idx.rb
-ruby bin/pnt-idx.rb
-ruby bin/tim-idx.rb
--- marimo/db/migrate/20090507103059_add_md5_to_tso.rb
+++
@@ -1,9 +0,0 @@
-class AddMd5ToTso < ActiveRecord::Migration
-  def self.up
-    add_column :tsos, :md5, :string, :limit => 32, :null => false
-  end
-
-  def self.down
-    remove_column :tsos, :md5
-  end
-end
