b9d5107c899f81a03802cd24d68775889ea466ab
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2013-01-04 05:26:49 +0000

    TSODeform: support fig file

--- tsoview/TSODeform/Program.cs
+++ tsoview/TSODeform/Program.cs
@@ -15,11 +15,17 @@ public static class Program
     {
         if (args.Length < 2)
         {
-            Console.WriteLine("TSODeform.exe <tso file> <tmo file>");
+            Console.WriteLine("TSODeform.exe <tso file> <tmo file> [fig file]");
             return;
         }
         string tso_file = args[0];
         string tmo_file = args[1];
+
+        string fig_file = null;
+        if (args.Length > 2)
+        {
+            fig_file = args[2];
+        }
 
         TSOFile tso = new TSOFile();
         tso.Load(tso_file);
@@ -30,6 +36,28 @@ public static class Program
         Figure fig = new Figure();
         fig.TSOList.Add(tso);
         fig.Tmo = tmo;
+
+        if (fig_file != null)
+        {
+            List<float> ratios = ReadFloats(fig_file);
+            /*
+            ◆FIGU
+            スライダの位置。値は float型で 0.0 .. 1.0
+                0: 姉妹
+                1: うで
+                2: あし
+                3: 胴まわり
+                4: おっぱい
+                5: つり目たれ目
+                6: やわらか
+             */
+            fig.slider_matrix.TallRatio = ratios[0];
+            fig.slider_matrix.ArmRatio = ratios[1];
+            fig.slider_matrix.LegRatio = ratios[2];
+            fig.slider_matrix.WaistRatio = ratios[3];
+            fig.slider_matrix.BustRatio = ratios[4];
+            fig.slider_matrix.EyeRatio = ratios[5];
+        }
         fig.UpdateNodeMapAndBoneMatrices();
 
         foreach (TSOMesh mesh in tso.meshes)
@@ -75,6 +103,18 @@ public static class Program
             nor += Vector3.TransformCoordinate(v.normal, m) * w;
         }
         v.normal = Vector3.Normalize(nor);
+    } 
+
+    static List<float> ReadFloats(string dest_file)
+    {
+        List<float> floats = new List<float>();
+        string line;
+        using (StreamReader source = new StreamReader(File.OpenRead(dest_file)))
+        while ((line = source.ReadLine()) != null)
+        {
+            floats.Add(Single.Parse(line));
+        }
+        return floats;
     }
 }
 }
