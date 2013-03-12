8aa3a0aa0a79e7d21630bdbc3612a02d4f32301f
nomeu <nomeu@72ae2088-5f33-de11-a17b-0000e250a282>
2013-03-12 08:50:30 +0000

    tagging Tso2Pmx-034

---
+++ tags/Tso2Pmx-034/Tmo2Vpd/Tmo2Vpd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+using TSOView;
+
+namespace Tmo2Vpd
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            TSOConfig tso_config;
+
+            string tso_config_file = Path.Combine(Application.StartupPath, @"config.xml");
+            if (File.Exists(tso_config_file))
+                tso_config = TSOConfig.Load(tso_config_file);
+            else
+                tso_config = new TSOConfig();
+
+            string[] str = { Path.Combine(Application.StartupPath, @"default.tdcgsav.png") };
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TSOForm(tso_config, str));
+        }
+    }
+}
